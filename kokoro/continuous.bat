7b71135d3f49e46af131661e890a5e8f33a4d523
Appu Goundan <appu@google.com>
2017-08-01 16:19:02 -0400

    Add kokoro build files (#3)
    * Add kokoro build files
    * make scripts executable
    * add maven src to get build working

---
+++ kokoro/continuous.bat
@@ -0,0 +1,10 @@
+@echo on
+
+cd github/minikube-build-tools-for-java
+
+rem there is no minikube installed on this system, so any integration test will probably not work
+rem we porbably also have to install VirtualBox or some other VM driver, it not even clear to me
+rem that kokoro will allow us to run a VM.
+
+cd minikube-gradle-plugin && call gradlew.bat clean build && cd ../minikube-maven-plugin && call mvnw.bat clean install
+exit /b %ERRORLEVEL%
---
+++ kokoro/continuous.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd github/minikube-build-tools-for-java
+
+(cd minikube-gradle-plugin; ./gradlew clean build)
+(cd minikube-maven-plugin; ./mvnw clean install)
