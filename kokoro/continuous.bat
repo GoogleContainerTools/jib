ff47a63c502b4d99845c65ccbd1194cfa36824a9
Qingyang Chen <qingyangc@google.com>
2017-11-20 16:28:30 -0500

    Updates Kokoro scripts to include testing crepecake. (#26)

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -6,5 +6,7 @@ rem there is no minikube installed on this system, so any integration test will 
 rem we porbably also have to install VirtualBox or some other VM driver, it not even clear to me
 rem that kokoro will allow us to run a VM.
 
-cd minikube-gradle-plugin && call gradlew.bat clean build && cd ../minikube-maven-plugin && call mvnw.bat clean install
+cd minikube-gradle-plugin && call gradlew.bat clean build && ^
+cd ../minikube-maven-plugin && call mvnw.bat clean install && ^
+cd ../crepecake && call gradlew.bat clean build
 exit /b %ERRORLEVEL%
--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -7,3 +7,4 @@ cd github/minikube-build-tools-for-java
 
 (cd minikube-gradle-plugin; ./gradlew clean build)
 (cd minikube-maven-plugin; ./mvnw clean install)
+(cd crepecake; ./gradlew clean build)
