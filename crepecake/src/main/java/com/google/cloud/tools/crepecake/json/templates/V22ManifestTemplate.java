2ba8ef14682b58072e29df23ca198b12d6553441
Qingyang Chen <qingyangc@google.com>
2017-12-13 14:33:13 -0500

    Adds JSON template for V2.1, V2.2 manifest schema and container configuration. (#46)

---
+++ crepecake/src/main/java/com/google/cloud/tools/crepecake/json/templates/V22ManifestTemplate.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.tools.crepecake.json.templates;
+
+import com.google.cloud.tools.crepecake.image.DescriptorDigest;
+import com.google.cloud.tools.crepecake.json.JsonTemplate;
+import com.google.common.annotations.VisibleForTesting;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * JSON Template for Docker Manifest Schema V2.2
+ *
+ * <p>Example manifest JSON:
+ *
+ * <pre>{@code
+ * {
+ *   "schemaVersion": 2,
+ *   "mediaType": "application/vnd.docker.distribution.manifest.v2+json",
+ *   "config": {
+ *     "mediaType": "application/vnd.docker.container.image.v1+json",
+ *     "size": "631,
+ *     "digest": "sha256:26b84ca5b9050d32e68f66ad0f3e2bbcd247198a6e6e09a7effddf126eb8d873"
+ *   },
+ *   "layers": [
+ *     {
+ *       "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
+ *       "size": 1991435,
+ *       "digest": "sha256:b56ae66c29370df48e7377c8f9baa744a3958058a766793f821dadcb144a4647"
+ *     },
+ *     {
+ *       "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
+ *       "size": 32,
+ *       "digest": "sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4"
+ *     }
+ *   ]
+ * }
+ * }</pre>
+ *
+ * @see <a href="https://docs.docker.com/registry/spec/manifest-v2-2/">Image Manifest Version 2,
+ *     Schema 2</a>
+ */
+public class V22ManifestTemplate extends JsonTemplate {
+
+  public static final String MEDIA_TYPE = "application/vnd.docker.distribution.manifest.v2+json";
+
+  private final int schemaVersion = 2;
+  private final String mediaType = MEDIA_TYPE;
+
+  /** The container configuration reference. */
+  private final ContainerConfigurationObjectTemplate config =
+      new ContainerConfigurationObjectTemplate();
+
+  /** The list of layer references. */
+  private final List<LayerObjectTemplate> layers = new ArrayList<>();
+
+  /** Template for inner JSON object representing the container configuration reference. */
+  private static class ContainerConfigurationObjectTemplate extends JsonTemplate {
+
+    private final String mediaType = "application/vnd.docker.container.image.v1+json";
+
+    private DescriptorDigest digest;
+    private int size;
+  }
+
+  /**
+   * Template for inner JSON object representing a layer as part of the list of layer references.
+   */
+  private static class LayerObjectTemplate extends JsonTemplate {
+
+    private final String mediaType = "application/vnd.docker.image.rootfs.diff.tar.gzip";
+
+    private DescriptorDigest digest;
+    private int size;
+  }
+
+  public void setContainerConfiguration(DescriptorDigest digest, int size) {
+    config.digest = digest;
+    config.size = size;
+  }
+
+  public void addLayer(DescriptorDigest digest, int size) {
+    LayerObjectTemplate layerObjectTemplate = new LayerObjectTemplate();
+    layerObjectTemplate.digest = digest;
+    layerObjectTemplate.size = size;
+    layers.add(layerObjectTemplate);
+  }
+
+  @VisibleForTesting
+  DescriptorDigest getContainerConfigurationDigest() {
+    return config.digest;
+  }
+
+  @VisibleForTesting
+  int getContainerConfigurationSize() {
+    return config.size;
+  }
+
+  @VisibleForTesting
+  DescriptorDigest getLayerDigest(int index) {
+    return layers.get(index).digest;
+  }
+
+  @VisibleForTesting
+  int getLayerSize(int index) {
+    return layers.get(index).size;
+  }
+}
---
+++ crepecake/src/test/java/com/google/cloud/tools/crepecake/json/templates/V22ManifestTemplateTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.tools.crepecake.json.templates;
+
+import com.google.cloud.tools.crepecake.image.DescriptorDigest;
+import com.google.cloud.tools.crepecake.json.JsonHelper;
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.security.DigestException;
+import org.junit.Assert;
+import org.junit.Test;
+
+/** Tests for {@link V22ManifestTemplate}. */
+public class V22ManifestTemplateTest {
+
+  @Test
+  public void testToJson() throws DigestException, IOException, URISyntaxException {
+    // Loads the expected JSON string.
+    File jsonFile =
+        new File(getClass().getClassLoader().getResource("json/v22manifest.json").toURI());
+    final String expectedJson =
+        CharStreams.toString(new InputStreamReader(new FileInputStream(jsonFile), Charsets.UTF_8));
+
+    // Creates the JSON object to serialize.
+    V22ManifestTemplate manifestJson = new V22ManifestTemplate();
+
+    manifestJson.setContainerConfiguration(
+        DescriptorDigest.fromDigest(
+            "sha256:8c662931926fa990b41da3c9f42663a537ccd498130030f9149173a0493832ad"),
+        1000);
+
+    manifestJson.addLayer(
+        DescriptorDigest.fromHash(
+            "4945ba5011739b0b98c4a41afe224e417f47c7c99b2ce76830999c9a0861b236"),
+        1000_000);
+
+    // Serializes the JSON object.
+    ByteArrayOutputStream jsonStream = new ByteArrayOutputStream();
+    JsonHelper.writeJson(jsonStream, manifestJson);
+
+    Assert.assertEquals(expectedJson, jsonStream.toString());
+  }
+
+  @Test
+  public void testFromJson() throws IOException, URISyntaxException, DigestException {
+    // Loads the JSON string.
+    File jsonFile =
+        new File(getClass().getClassLoader().getResource("json/v22manifest.json").toURI());
+
+    // Deserializes into a manifest JSON object.
+    V22ManifestTemplate manifestJson =
+        JsonHelper.readJsonFromFile(jsonFile, V22ManifestTemplate.class);
+
+    Assert.assertEquals(
+        DescriptorDigest.fromDigest(
+            "sha256:8c662931926fa990b41da3c9f42663a537ccd498130030f9149173a0493832ad"),
+        manifestJson.getContainerConfigurationDigest());
+
+    Assert.assertEquals(1000, manifestJson.getContainerConfigurationSize());
+
+    Assert.assertEquals(
+        DescriptorDigest.fromHash(
+            "4945ba5011739b0b98c4a41afe224e417f47c7c99b2ce76830999c9a0861b236"),
+        manifestJson.getLayerDigest(0));
+
+    Assert.assertEquals(1000_000, manifestJson.getLayerSize(0));
+  }
+}
