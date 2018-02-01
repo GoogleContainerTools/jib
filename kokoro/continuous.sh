d6fb28d3aa2820ca017c26000e4e319a017bdb73
Qingyang Chen <qingyangc@google.com>
2018-02-01 18:30:32 -0500

    Fixes Kokoro build.

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -5,5 +5,5 @@ set -x
 
 cd github/jib
 
-(cd jib-core; ./gradlew clean build --info)
+(cd jib-core; ./gradlew clean build publishToMavenLocal--info)
 (cd jib-maven-plugin; ./mvnw clean install cobertura:cobertura -B -U -X)
