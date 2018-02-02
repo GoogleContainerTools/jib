738b40f20efa2181fb4747b4b0f0da1a65dec582
Qingyang Chen <qingyangc@google.com>
2018-02-02 13:50:42 -0500

    Fixes batch script.

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -6,7 +6,7 @@ set PATH=%JAVA_HOME%\bin;%PATH%
 
 cd github/jib
 
-cd jib-core && call gradlew.bat clean build publishToMavenLocal --info
+cd jib-core && call gradlew.bat clean build publishToMavenLocal --info && ^
 cd ../jib-maven-plugin && call mvnw.cmd clean install cobertura:cobertura -B -U -X
 
 exit /b %ERRORLEVEL%
