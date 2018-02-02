7adc55ce62293b67f7c78fd53eeefe721cff7d1d
Qingyang Chen <qingyangc@google.com>
2018-02-02 13:04:21 -0500

    Use Java 8 for Kokoro windows.

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -1,9 +1,11 @@
 @echo on
 
+set JAVA_HOME=c:\program files\java\jdk1.8.0_152
+set PATH=%JAVA_HOME%\bin;%PATH%
+
 cd github/jib
 
 cd jib-core && call gradlew.bat clean build publishToMavenLocal --info
 cd ../jib-maven-plugin && call mvnw.cmd clean install cobertura:cobertura -B -U -X
 
 exit /b %ERRORLEVEL%
-g
