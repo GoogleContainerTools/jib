3d657215db142baa08a57b9a0096d28b44f02d74
Qingyang Chen <qingyangc@google.com>
2018-02-12 16:59:00 -0500

    Fixing.

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -7,7 +7,7 @@ set -x
 sudo ln -s /Applications/Docker.app/Contents/Resources/bin/docker /usr/local/bin/docker
 
 # Stops any left-over containers.
-docker stop $(docker container ls --quiet)
+docker stop $(docker container ls --quiet) || true
 
 cd github/jib
 
