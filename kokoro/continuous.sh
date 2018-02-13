39542ddc2233b9664166492d91c5ce60c7e98abf
Qingyang Chen <qingyangc@google.com>
2018-02-13 18:59:17 -0500

    .

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -6,6 +6,8 @@ set -x
 # For MacOS builds, link Docker to run as 'docker'.
 export PATH=$PATH:/Applications/Docker.app/Contents/Resources/bin
 
+gcloud components install docker-credential-gcr
+
 # Stops any left-over containers.
 docker stop $(docker container ls --quiet) || true
 
