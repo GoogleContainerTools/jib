f7c21a5f87ea4beb07d0b9cc15e88da3536321b0
Qingyang Chen <qingyangc@google.com>
2018-02-13 19:21:22 -0500

    .

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -9,8 +9,10 @@ export PATH=$PATH:/Applications/Docker.app/Contents/Resources/bin
 which docker
 ls -l /usr/local/bin/gcloud
 ls /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/
+gcloud components install docker-credential-gcr
+ls /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/
+which docker-credential-gcr
 which gcloud && false
-#gcloud components install docker-credential-gcr
 
 # Stops any left-over containers.
 docker stop $(docker container ls --quiet) || true
