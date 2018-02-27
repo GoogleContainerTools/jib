a44d2fdfcf0d33f48225bb63a29ce1dd46219fb9
Qingyang Chen <qingyangc@google.com>
2018-02-27 14:10:46 -0500

    Enables Kokoro integration tests for jib-maven-plugin.

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 set -e
+
+docker login -u _json_key -p ${JIB_INTEGRATION_TESTING_KEY} https://gcr.io
+
 set -x
 
 gcloud components install docker-credential-gcr
@@ -12,4 +15,4 @@ docker stop $(docker container ls --quiet) || true
 cd github/jib
 
 (cd jib-core; ./gradlew clean build integrationTest publishToMavenLocal --info)
-(cd jib-maven-plugin; ./mvnw clean install cobertura:cobertura -B -U -X)
+(cd jib-maven-plugin; ./mvnw clean install cobertura:cobertura -P integration-tests -B -U -X)
