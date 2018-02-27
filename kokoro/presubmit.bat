e8435c66afa3d1126f2c64766a9f5f6b58fe902e
Qingyang Chen <qingyangc@google.com>
2018-02-27 10:23:41 -0500

    Separates presubmit from continuous build scripts. (#122)

---
+++ kokoro/presubmit.bat
@@ -0,0 +1,16 @@
+@echo on
+
+REM Java 9 does not work with Mockito mockmaker.
+set JAVA_HOME=c:\program files\java\jdk1.8.0_152
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+cd github/jib
+
+REM Stops any left-over containers.
+REM FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker rm -vf %%i
+
+REM TODO: Enable integration tests once docker works (b/73345382).
+cd jib-core && call gradlew.bat clean build publishToMavenLocal --info && ^
+cd ../jib-maven-plugin && call mvnw.cmd clean install cobertura:cobertura -B -U -X
+
+exit /b %ERRORLEVEL%
