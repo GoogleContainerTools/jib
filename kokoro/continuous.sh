7c77d829f5df78e9ca9eb793d85ca375da794804
Qingyang Chen <qingyangc@google.com>
2018-02-27 18:52:16 -0500

    testing

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -8,9 +8,7 @@ docker login -u _json_key -p "${JIB_INTEGRATION_TESTING_KEY}" https://gcr.io
 set -x
 
 export PATH=$PATH:/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/
-
-docker-credential-gcr list
-cat $HOME/.docker/config.json
+echo $JIB_INTEGRATION_TESTING_KEY
 
 # Stops any left-over containers.
 docker stop $(docker container ls --quiet) || true
