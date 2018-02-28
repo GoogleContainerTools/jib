203cddfb40d5d802a8fab2d6c56dfc3eb2a156eb
Qingyang Chen <qingyangc@google.com>
2018-02-28 14:41:52 -0500

    hmm

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -12,7 +12,7 @@ gcloud components install docker-credential-gcr
 export GOOGLE_APPLICATION_CREDENTIALS=./keyfile.json
 docker-credential-gcr configure-docker
 
-export PATH=$PATH:/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/
+# export PATH=$PATH:/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/
 
 # Stops any left-over containers.
 docker stop $(docker container ls --quiet) || true
@@ -20,8 +20,4 @@ docker stop $(docker container ls --quiet) || true
 cd github/jib
 
 (cd jib-core; ./gradlew clean build integrationTest publishToMavenLocal --info)
-
-echo gcr.io | docker-credential-gcr get
-echo -n gcr.io | docker-credential-gcr get
-
 (cd jib-maven-plugin; ./mvnw clean install cobertura:cobertura -P integration-tests -B -U -X)
