0c6dc821f424866ed013b29453e851c6caa99d68
Qingyang Chen <qingyangc@google.com>
2018-02-28 19:16:34 -0500

    Fixes format.

--- jib-core/src/main/java/com/google/cloud/tools/jib/cache/CacheWriter.java
+++ jib-core/src/main/java/com/google/cloud/tools/jib/cache/CacheWriter.java
@@ -28,7 +28,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
