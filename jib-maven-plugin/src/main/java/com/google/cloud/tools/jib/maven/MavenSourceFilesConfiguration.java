3c968ee4110730d0cd5ef8a4105f05505071a6c2
Qingyang Chen <qingyangc@google.com>
2018-03-13 17:21:01 -0400

    Fixes format.

--- jib-maven-plugin/src/main/java/com/google/cloud/tools/jib/maven/MavenSourceFilesConfiguration.java
+++ jib-maven-plugin/src/main/java/com/google/cloud/tools/jib/maven/MavenSourceFilesConfiguration.java
@@ -26,8 +26,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
-
-import org.apache.maven.Maven;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.project.MavenProject;
 
--- jib-maven-plugin/src/test/java/com/google/cloud/tools/jib/maven/MavenSourceFilesConfigurationTest.java
+++ jib-maven-plugin/src/test/java/com/google/cloud/tools/jib/maven/MavenSourceFilesConfigurationTest.java
@@ -61,7 +61,8 @@ public class MavenSourceFilesConfigurationTest {
                 makeArtifact(Paths.get("application", "dependencies", "dependency-1.0.0.jar"))));
     Mockito.when(mockMavenProject.getArtifacts()).thenReturn(artifacts);
 
-    testMavenSourceFilesConfiguration = MavenSourceFilesConfiguration.getForProject(mockMavenProject);
+    testMavenSourceFilesConfiguration =
+        MavenSourceFilesConfiguration.getForProject(mockMavenProject);
   }
 
   @Test
