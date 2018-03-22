1a86dd64ff51e0e65c31b106a92e368012222840
Qingyang Chen <qingyangc@google.com>
2018-03-22 11:27:05 -0400

    Changing to extension.

--- jib-gradle-plugin/src/main/java/com/google/cloud/tools/jib/gradle/BuildImageTask.java
+++ jib-gradle-plugin/src/main/java/com/google/cloud/tools/jib/gradle/BuildImageTask.java
@@ -16,162 +16,35 @@
 
 package com.google.cloud.tools.jib.gradle;
 
-import com.google.common.annotations.VisibleForTesting;
-import groovy.lang.Closure;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import com.google.common.base.Preconditions;
 import javax.annotation.Nullable;
 import org.gradle.api.DefaultTask;
-import org.gradle.api.InvalidUserDataException;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
-import org.gradle.api.tasks.TaskValidationException;
-import org.gradle.util.ConfigureUtil;
 
-/**
- * Builds a container image.
- *
- * <p>Example configuration:
- *
- * <pre>{@code
- * jib {
- *   from {
- *     image = ‘gcr.io/my-gcp-project/my-base-image’
- *     credHelper = ‘gcr’
- *   }
- *   to {
- *     image = ‘gcr.io/gcp-project/my-app:built-with-jib’
- *     credHelper = ‘ecr-login’
- *   }
- *   jvmFlags = [‘-Xms512m’, ‘-Xdebug’]
- *   mainClass = ‘com.mycompany.myproject.Main’
- *   reproducible = true
- *   format = OCI
- * }
- * }</pre>
- */
+/** Builds a container image. */
 public class BuildImageTask extends DefaultTask {
 
-  /** Enumeration of supported image formats. */
-  private enum ImageFormat {
-    DOCKER,
-    OCI
-  }
-
-  /**
-   * Configures an image to be used in the build steps. This is configurable with Groovy closures.
-   */
-  @VisibleForTesting
-  class ImageConfiguration {
-
-    @Nullable private String image;
-    @Nullable private String credHelper;
-
-    @VisibleForTesting
-    @Nullable
-    String getImage() {
-      return image;
-    }
-
-    @VisibleForTesting
-    @Nullable
-    String getCredHelper() {
-      return credHelper;
-    }
-
-    /**
-     * @param closureName the name of the method the closure was passed to
-     * @param closure the closure to apply
-     */
-    private ImageConfiguration configure(String closureName, Closure closure) {
-      ConfigureUtil.configureSelf(closure, this);
-
-      // 'image' is a required property
-      if (image == null) {
-        // The wrapping mimics Gradle's built-in task configuration validation.
-        throw new TaskValidationException(
-            "A problem was found with the configuration of task '" + getName() + "'",
-            Collections.singletonList(
-                new InvalidUserDataException(
-                    "'" + closureName + "' closure must define 'image' property")));
-      }
-      return this;
-    }
-  }
-
-  @Nullable private ImageConfiguration from;
-  @Nullable private ImageConfiguration to;
-
-  private List<String> jvmFlags = new ArrayList<>();
-  @Nullable private String mainClass;
-  private boolean reproducible = true;
-  private ImageFormat format = ImageFormat.DOCKER;
-
-  /** Configures the base image. */
-  public void from(Closure<?> closure) {
-    from = new ImageConfiguration().configure("from", closure);
-  }
-
-  /** Configures the target image. */
-  public void to(Closure<?> closure) {
-    to = new ImageConfiguration().configure("to", closure);
-  }
-
-  @Nullable
-  @Input
-  @Optional
-  public ImageConfiguration getFrom() {
-    return from;
-  }
-
-  @Nullable
-  @Input
-  public ImageConfiguration getTo() {
-    return to;
-  }
-
-  @Input
-  public List<String> getJvmFlags() {
-    return jvmFlags;
-  }
-
-  public void setJvmFlags(List<String> jvmFlags) {
-    this.jvmFlags = jvmFlags;
-  }
-
-  @Input
-  @Optional
-  @Nullable
-  public String getMainClass() {
-    return mainClass;
-  }
-
-  public void setMainClass(@Nullable String mainClass) {
-    this.mainClass = mainClass;
-  }
-
-  @Input
-  public boolean getReproducible() {
-    return reproducible;
-  }
-
-  public void setReproducible(boolean isEnabled) {
-    reproducible = isEnabled;
-  }
-
-  @Input
-  public ImageFormat getFormat() {
-    return format;
-  }
-
-  public void setFormat(ImageFormat format) {
-    this.format = format;
-  }
+  /** Linked extension that configures this task. */
+  @Nullable private JibExtension extension;
 
   @TaskAction
   public void buildImage() {
+    Preconditions.checkNotNull(extension);
+
     // TODO: Implement.
+
+    System.out.println("from.image : " + extension.getFrom().getImage());
+    System.out.println("from.credHelper : " + extension.getFrom().getCredHelper());
+    System.out.println("to.image : " + extension.getTo().getImage());
+    System.out.println("to.credHelper : " + extension.getTo().getCredHelper());
+
+    System.out.println("jvmFlags: " + extension.getJvmFlags());
+    System.out.println("mainClass: " + extension.getMainClass());
+    System.out.println("reproducible: " + extension.getReproducible());
+    System.out.println("format: " + extension.getFormat());
+  }
+
+  void setExtension(JibExtension jibExtension) {
+    extension = jibExtension;
   }
 }
---
+++ jib-gradle-plugin/src/main/java/com/google/cloud/tools/jib/gradle/JibExtension.java
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.tools.jib.gradle;
+
+import java.util.Collections;
+import java.util.List;
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import org.gradle.api.Action;
+import org.gradle.api.Project;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ListProperty;
+import org.gradle.api.provider.Property;
+
+/**
+ * Plugin extension for {@link JibPlugin}.
+ *
+ * <p>Example configuration:
+ *
+ * <pre>{@code
+ * jib {
+ *   from {
+ *     image = ‘gcr.io/my-gcp-project/my-base-image’
+ *     credHelper = ‘gcr’
+ *   }
+ *   to {
+ *     image = ‘gcr.io/gcp-project/my-app:built-with-jib’
+ *     credHelper = ‘ecr-login’
+ *   }
+ *   jvmFlags = [‘-Xms512m’, ‘-Xdebug’]
+ *   mainClass = ‘com.mycompany.myproject.Main’
+ *   reproducible = true
+ *   format = OCI
+ * }
+ * }</pre>
+ */
+public class JibExtension {
+
+  /**
+   * Configures an image to be used in the build steps. This is configurable with Groovy closures.
+   */
+  public static class ImageConfiguration {
+
+    @Nullable private String image;
+    @Nullable private String credHelper;
+
+    public ImageConfiguration() {}
+
+    @Nullable
+    public String getImage() {
+      return image;
+    }
+
+    public void setImage(String image) {
+      this.image = image;
+    }
+
+    @Nullable
+    public String getCredHelper() {
+      return credHelper;
+    }
+
+    public void setCredHelper(String credHelper) {
+      this.credHelper = credHelper;
+    }
+
+    //    /**
+    //     * @param closureName the name of the method the closure was passed to
+    //     * @param closure the closure to apply
+    //     */
+    //    private ImageConfiguration configure(String closureName, Closure closure) {
+    //      ConfigureUtil.configureSelf(closure, this);
+    //
+    //      // 'image' is a required property
+    //      if (image == null) {
+    //        // The wrapping mimics Gradle's built-in task configuration validation.
+    //        throw new TaskValidationException(
+    //            "A problem was found with the configuration of task '" + getName() + "'",
+    //            Collections.singletonList(
+    //                new InvalidUserDataException(
+    //                    "'" + closureName + "' closure must define 'image' property")));
+    //      }
+    //      return this;
+    //    }
+  }
+
+  /** Enumeration of supported image formats. */
+  private enum ImageFormat {
+    DOCKER,
+    OCI
+  }
+
+  // Defines default configuration values.
+  private static final List<String> DEFAULT_JVM_FLAGS = Collections.emptyList();
+  private static final ImageFormat DEFAULT_FORMAT = ImageFormat.DOCKER;
+  private static final boolean DEFAULT_REPRODUCIBLE = true;
+
+  private final ImageConfiguration from;
+  private final ImageConfiguration to;
+  private final ListProperty<String> jvmFlags;
+  private final Property<String> mainClass;
+  private final Property<Boolean> reproducible;
+  private Property<ImageFormat> format;
+
+  @Inject
+  public JibExtension(Project project) {
+    ObjectFactory objectFactory = project.getObjects();
+
+    from = objectFactory.newInstance(ImageConfiguration.class);
+    to = objectFactory.newInstance(ImageConfiguration.class);
+
+    jvmFlags = objectFactory.listProperty(String.class);
+    mainClass = objectFactory.property(String.class);
+    reproducible = objectFactory.property(Boolean.class);
+    format = objectFactory.property(ImageFormat.class);
+  }
+
+  public void from(Action<? super ImageConfiguration> action) {
+    action.execute(from);
+  }
+
+  public void to(Action<? super ImageConfiguration> action) {
+    action.execute(to);
+  }
+
+  public List<String> getJvmFlags() {
+    return jvmFlags.getOrElse(DEFAULT_JVM_FLAGS);
+  }
+
+  public void setJvmFlags(List<String> jvmFlags) {
+    this.jvmFlags.set(jvmFlags);
+  }
+
+  @Nullable
+  public String getMainClass() {
+    return mainClass.getOrNull();
+  }
+
+  public void setMainClass(String mainClass) {
+    this.mainClass.set(mainClass);
+  }
+
+  public boolean getReproducible() {
+    return reproducible.getOrElse(DEFAULT_REPRODUCIBLE);
+  }
+
+  public void setReproducible(boolean isEnabled) {
+    reproducible.set(isEnabled);
+  }
+
+  public ImageFormat getFormat() {
+    return format.getOrElse(DEFAULT_FORMAT);
+  }
+
+  public void setFormat(ImageFormat format) {
+    this.format.set(format);
+  }
+
+  ImageConfiguration getFrom() {
+    return from;
+  }
+
+  ImageConfiguration getTo() {
+    return to;
+  }
+}
