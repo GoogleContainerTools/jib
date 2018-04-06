9f14488b910729ba6cef0463cc159264795ce321
Qingyang Chen <qingyangc@google.com>
2018-04-06 16:35:11 +0200

    Adds prepare_release script. (#211)

---
+++ jib-gradle-plugin/scripts/prepare_release.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -
+# Usage: ./scripts/prepare_release.sh <release version>
+
+set -e
+
+Colorize() {
+	echo "$(tput setff $2)$1$(tput sgr0)"
+}
+
+EchoRed() {
+	echo "$(tput setaf 1; tput bold)$1$(tput sgr0)"
+}
+EchoGreen() {
+	echo "$(tput setaf 2; tput bold)$1$(tput sgr0)"
+}
+
+Die() {
+	EchoRed "$1"
+	exit 1
+}
+
+DieUsage() {
+    Die "Usage: ./scripts/prepare_release.sh <release version>"
+}
+
+# Usage: CheckVersion <version>
+CheckVersion() {
+    [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] || Die "Version not in ###.###.### format."
+}
+
+[ $# -ne 2 ] || DieUsage
+
+EchoGreen '===== RELEASE SETUP SCRIPT ====='
+
+VERSION=$1
+CheckVersion ${VERSION}
+
+if [[ $(git status -uno --porcelain) ]]; then
+    Die 'There are uncommitted changes.'
+fi
+
+# Checks out a new branch for this version release (eg. 1.5.7).
+git checkout -b gradle_release_v${VERSION}
+
+# Changes the version for release and creates the commits/tags.
+echo | ./gradlew release -PreleaseVersion=${VERSION}
+
+# Pushes the release branch and tag to Github.
+git push origin gradle_release_v${VERSION}
+git push origin v${VERSION}-gradle
+
+# File a PR on Github for the new branch. Have someone LGTM it, which gives you permission to continue.
+EchoGreen 'File a PR for the new release branch:'
+echo https://github.com/google/jib/compare/gradle_release_v${VERSION}
+
+EchoGreen "Once approved and merged, checkout the 'v${VERSION}-gradle' tag and run './gradlew publishPlugins'."
