4265673237d6fa330c9046a953b4e7e85e8723a7
Qingyang Chen <qingyangc@google.com>
2018-06-01 10:50:33 -0400

    Attaches original IOException from writing to stdin as cause.

--- jib-core/src/main/java/com/google/cloud/tools/jib/docker/DockerClient.java
+++ jib-core/src/main/java/com/google/cloud/tools/jib/docker/DockerClient.java
@@ -31,6 +31,20 @@ import java.util.function.Function;
 /** Calls out to the {@code docker} CLI. */
 public class DockerClient {
 
+  /** Accepts an {@link OutputStream}, possibly throwing an {@link IOException}. */
+  @FunctionalInterface
+  private interface OutputStreamConsumer {
+
+    void accept(OutputStream outputStream) throws IOException;
+  }
+
+  /** Handles an {@link IOException}, possibly throwing an {@link IOException}. */
+  @FunctionalInterface
+  private interface IOExceptionHandler {
+
+    void handle(IOException ex) throws IOException;
+  }
+
   /**
    * @param dockerSubCommand the subcommand to run after {@code docker}
    * @return the default {@link ProcessBuilder} factory for running a {@code docker} subcommand
@@ -40,6 +54,26 @@ public class DockerClient {
     dockerCommand.add("docker");
     dockerCommand.addAll(dockerSubCommand);
     return new ProcessBuilder(dockerCommand);
+  }
+
+  /**
+   * Writes a process's stdin.
+   *
+   * @param process the {@link Process} to
+   * @param stdinConsumer writes to a stdin {@link OutputStream}
+   * @param ioExceptionHandler handles any {@link IOException} returned by the {@code stdinConsumer}
+   */
+  private static void writeToStdin(
+      Process process, OutputStreamConsumer stdinConsumer, IOExceptionHandler ioExceptionHandler)
+      throws IOException {
+    try (OutputStream stdin = process.getOutputStream()) {
+      try {
+        stdinConsumer.accept(stdin);
+
+      } catch (IOException ex) {
+        ioExceptionHandler.handle(ex);
+      }
+    }
   }
 
   /** Factory for generating the {@link ProcessBuilder} for running {@code docker} commands. */
@@ -79,18 +113,18 @@ public class DockerClient {
     // Runs 'docker load'.
     Process dockerProcess = docker("load");
 
-    try (OutputStream stdin = dockerProcess.getOutputStream()) {
-      imageTarballBlob.writeTo(stdin);
-
-    } catch (IOException ex) {
-      // This error usually happens when docker is not able to connect to the daemon socket.
-      try (InputStreamReader stderr =
-          new InputStreamReader(dockerProcess.getErrorStream(), StandardCharsets.UTF_8)) {
-        String error = CharStreams.toString(stderr);
-
-        throw new IOException("'docker load' command failed with error: " + error);
-      }
-    }
+    writeToStdin(
+        dockerProcess,
+        imageTarballBlob::writeTo,
+        ioException -> {
+          // Tries to read from stderr.
+          try (InputStreamReader stderr =
+              new InputStreamReader(dockerProcess.getErrorStream(), StandardCharsets.UTF_8)) {
+            throw new IOException(
+                "'docker load' command failed with error: " + CharStreams.toString(stderr),
+                ioException);
+          }
+        });
 
     try (InputStreamReader stdout =
         new InputStreamReader(dockerProcess.getInputStream(), StandardCharsets.UTF_8)) {
