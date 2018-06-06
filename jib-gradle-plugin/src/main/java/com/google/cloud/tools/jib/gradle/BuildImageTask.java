67acb495c420e709728614c1692e6757ccdc9f81
Tad Cordle <tcordle@google.com>
2018-06-06 16:15:45 -0400

    Move disableHttpLogging to BuildLoggers

--- jib-gradle-plugin/src/main/java/com/google/cloud/tools/jib/gradle/BuildImageTask.java
+++ jib-gradle-plugin/src/main/java/com/google/cloud/tools/jib/gradle/BuildImageTask.java
@@ -105,7 +105,7 @@ public class BuildImageTask extends DefaultTask {
             .build();
 
     // TODO: Instead of disabling logging, have authentication credentials be provided
-    GradleProjectProperties.disableHttpLogging();
+    GradleBuildLogger.disableHttpLogging();
 
     RegistryClient.setUserAgentSuffix(USER_AGENT_SUFFIX);
 
