6298c96ee5ae81bf784471d51f6f16b98fa1c0d2
Qingyang Chen <qingyangc@google.com>
2018-06-20 12:33:45 -0400

    Fixes typo.

--- jib-core/src/main/java/com/google/cloud/tools/jib/registry/RegistryEndpointCaller.java
+++ jib-core/src/main/java/com/google/cloud/tools/jib/registry/RegistryEndpointCaller.java
@@ -149,8 +149,8 @@ class RegistryEndpointCaller<T> {
 
         } else if (httpResponseException.getStatusCode()
             == HttpStatusCodes.STATUS_CODE_TEMPORARY_REDIRECT) {
-          // 'Localion' header can be relative or absolute.
-          // TODO: Add test.
+          // 'Location' header can be relative or absolute.
+          // TODO: Add test after #407 is merged.
           URL redirectLocation =
               new URL(requestState.url, httpResponseException.getHeaders().getLocation());
           // TODO: Use copy-construct builder.
