dd76c5ee875eb3333f5251efe5af69ed08762039
Qingyang Chen <qingyangc@google.com>
2018-07-02 10:41:44 -0400

    Removes LayerConfiguration#Builder.

--- jib-core/src/main/java/com/google/cloud/tools/jib/configuration/LayerConfiguration.java
+++ jib-core/src/main/java/com/google/cloud/tools/jib/configuration/LayerConfiguration.java
@@ -20,65 +20,28 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import java.nio.file.Path;
 import java.util.List;
-import javax.annotation.Nullable;
 
 /** Configures how to build a layer in the container image. */
 // TODO: Consolidate with ReproducibleLayerBuilder#LayerEntry.
 public class LayerConfiguration {
 
-  /** Builds a {@link LayerConfiguration}. */
-  public static class Builder {
-
-    @Nullable private ImmutableList<Path> sourceFiles;
-    @Nullable private String destinationOnImage;
-
-    private Builder() {}
-
-    /**
-     * Sets the source files to build from. Source files that are directories will have all subfiles
-     * in the directory added (but not the directory itself).
-     *
-     * <p>The source files are specified as a list instead of a set to define the order in which
-     * they are added.
-     *
-     * @param sourceFiles the source files to build from
-     * @return this
-     */
-    public Builder setSourceFiles(List<Path> sourceFiles) {
-      Preconditions.checkArgument(!sourceFiles.contains(null));
-      this.sourceFiles = ImmutableList.copyOf(sourceFiles);
-      return this;
-    }
-
-    /**
-     * The Unix-style path to add the source files to in the container image filesystem.
-     *
-     * @param destinationOnImage the destination on the container image
-     * @return this
-     */
-    public Builder setDestinationOnImage(String destinationOnImage) {
-      this.destinationOnImage = destinationOnImage;
-      return this;
-    }
-
-    public LayerConfiguration build() {
-      if (sourceFiles == null || destinationOnImage == null) {
-        throw new IllegalStateException("Required fields should not be null");
-      }
-
-      return new LayerConfiguration(sourceFiles, destinationOnImage);
-    }
-  }
-
-  public static Builder builder() {
-    return new Builder();
-  }
-
   private final ImmutableList<Path> sourceFiles;
   private final String destinationOnImage;
 
-  private LayerConfiguration(ImmutableList<Path> sourceFiles, String destinationOnImage) {
-    this.sourceFiles = sourceFiles;
+  /**
+   * Constructs a new layer configuration.
+   *
+   * <p>The source files are specified as a list instead of a set to define the order in which they
+   * are added.
+   *
+   * @param sourceFiles the source files to build from. Source files that are directories will have
+   *     all subfiles in the directory added (but not the directory itself)
+   * @param destinationOnImage Unix-style path to add the source files to in the container image
+   *     filesystem
+   */
+  public LayerConfiguration(List<Path> sourceFiles, String destinationOnImage) {
+    Preconditions.checkArgument(!sourceFiles.contains(null));
+    this.sourceFiles = ImmutableList.copyOf(sourceFiles);
     this.destinationOnImage = destinationOnImage;
   }
 
