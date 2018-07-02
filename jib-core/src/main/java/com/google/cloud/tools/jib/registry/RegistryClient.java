b1ff6d917950e0688e131f11b10554c448187743
Qingyang Chen <qingyangc@google.com>
2018-07-02 16:56:33 -0400

    Adds metadata to plugin JARs.

--- jib-core/src/main/java/com/google/cloud/tools/jib/registry/RegistryClient.java
+++ jib-core/src/main/java/com/google/cloud/tools/jib/registry/RegistryClient.java
@@ -115,6 +115,8 @@ public class RegistryClient {
   }
 
   /**
+   * The {@code User-Agent} is in the form of {@code jib <version> <type>}. For example: {@code jib 0.9.0 jib-maven-plugin}.
+   *
    * @return the {@code User-Agent} header to send. The {@code User-Agent} can be disabled by
    *     setting the system property variable {@code _JIB_DISABLE_USER_AGENT} to any non-empty
    *     string.
