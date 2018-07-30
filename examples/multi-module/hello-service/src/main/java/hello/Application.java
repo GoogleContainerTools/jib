56b34b04ecb804267215772946e0693b7f1c8b17
Qingyang Chen <qingyangc@google.com>
2018-07-30 16:45:22 -0400

    Changes all indentation to 2 spaces.

--- examples/multi-module/hello-service/src/main/java/hello/Application.java
+++ examples/multi-module/hello-service/src/main/java/hello/Application.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+    SpringApplication.run(Application.class, args);
+  }
 }
