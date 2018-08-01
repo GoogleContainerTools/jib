7abda043d1bd5555e72e849c55b9852d312f91f5
Appu Goundan <appu@google.com>
2018-08-01 16:16:42 -0400

    Narrow down tests + copyright + comments

--- jib-core/src/test/java/com/google/cloud/tools/jib/configuration/ContainerConfigurationTest.java
+++ jib-core/src/test/java/com/google/cloud/tools/jib/configuration/ContainerConfigurationTest.java
@@ -1,15 +1,31 @@
-package com.google.cloud.tools.jib.configuration;
+/*
+ * Copyright 2018 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
-import static org.junit.Assert.*;
+package com.google.cloud.tools.jib.configuration;
 
 import com.google.cloud.tools.jib.configuration.Port.Protocol;
 import com.google.common.collect.ImmutableMap;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 
+/** Tests for {@link ContainerConfiguration}. */
 public class ContainerConfigurationTest {
 
   @Test
@@ -31,9 +47,9 @@ public class ContainerConfigurationTest {
     }
 
     // Exposed ports element should not be null.
+    List<Port> badPorts = Arrays.asList(new Port(1000, Protocol.TCP), null);
     try {
-      ContainerConfiguration.builder()
-          .setExposedPorts(Arrays.asList(new Port(1000, Protocol.TCP), null));
+      ContainerConfiguration.builder().setExposedPorts(badPorts);
       Assert.fail("The IllegalArgumentException should be thrown.");
     } catch (IllegalArgumentException ex) {
       Assert.assertNull(ex.getMessage());
