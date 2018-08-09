3d9b4589ece04ca12eabdfdf8321766ca56ec6bf
Tad Cordle <tcordle@google.com>
2018-08-09 17:51:32 -0400

    Format

--- jib-core/src/integration-test/java/com/google/cloud/tools/jib/registry/LocalRegistry.java
+++ jib-core/src/integration-test/java/com/google/cloud/tools/jib/registry/LocalRegistry.java
@@ -23,7 +23,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
 import javax.annotation.Nullable;
 import org.junit.rules.ExternalResource;
