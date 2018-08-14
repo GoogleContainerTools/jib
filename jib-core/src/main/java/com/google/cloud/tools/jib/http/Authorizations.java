f30dcbec4d420b96d15abd046dbca6e60a938eba
Q Chen <qingyangc@google.com>
2018-08-14 14:39:04 -0400

    Fixes Base64 encoding to string. (#838)

--- jib-core/src/main/java/com/google/cloud/tools/jib/http/Authorizations.java
+++ jib-core/src/main/java/com/google/cloud/tools/jib/http/Authorizations.java
@@ -37,10 +37,7 @@ public class Authorizations {
    */
   public static Authorization withBasicCredentials(String username, String secret) {
     String credentials = username + ":" + secret;
-    String token =
-        new String(
-            Base64.encodeBase64(credentials.getBytes(StandardCharsets.UTF_8)),
-            StandardCharsets.UTF_8);
+    String token = Base64.encodeBase64String(credentials.getBytes(StandardCharsets.UTF_8));
     return new Authorization("Basic", token);
   }
 
