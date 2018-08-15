2de90b3d3bfc787e4b4927d4e3f63ba1537baf54
Tad Cordle <tcordle@google.com>
2018-08-15 11:24:19 -0400

    Change missing build output directory message

--- jib-gradle-plugin/src/test/java/com/google/cloud/tools/jib/gradle/GradleLayerConfigurationsTest.java
+++ jib-gradle-plugin/src/test/java/com/google/cloud/tools/jib/gradle/GradleLayerConfigurationsTest.java
@@ -162,7 +162,8 @@ public class GradleLayerConfigurationsTest {
         mockProject, mockGradleJibLogger, Paths.get("nonexistent/path"));
 
     Mockito.verify(mockGradleJibLogger)
-        .warn("Could not find build output directory '" + nonexistentFile + "'");
+        .info("Adding corresponding output directories of source sets to image.");
+    Mockito.verify(mockGradleJibLogger).info("\t'" + nonexistentFile + "' (not found, skipped)");
     Mockito.verify(mockGradleJibLogger)
         .warn("No classes files were found - did you compile your project?");
   }
