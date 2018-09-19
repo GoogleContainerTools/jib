61ceb1efb41dea13f7b606dca9e7b42d058f7afe
Tad Cordle <tcordle@google.com>
2018-09-19 12:09:42 -0400

    Add _jibSkaffoldFiles task that works for simple projects

---
+++ jib-gradle-plugin/src/integration-test/java/com/google/cloud/tools/jib/gradle/FilesTaskTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.tools.jib.gradle;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.BuildTask;
+import org.gradle.testkit.runner.TaskOutcome;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public class FilesTaskTest {
+
+  @ClassRule public static final TestProject simpleTestProject = new TestProject("simple");
+
+  @ClassRule public static final TestProject multiTestProject = new TestProject("multi-service");
+
+  @Test
+  public void testFilesTask_singleProject() {
+    Path projectRoot = simpleTestProject.getProjectRoot();
+    verifyFiles(
+        simpleTestProject,
+        ImmutableList.of(
+            projectRoot.resolve("build.gradle"),
+            projectRoot.resolve("src/main/java"),
+            projectRoot.resolve("src/main/resources"),
+            projectRoot.resolve("src/main/custom-extra-dir"),
+            projectRoot.resolve("libs/dependency-1.0.0.jar")));
+  }
+
+  @Test
+  public void testFilesTask_multiProjectSimpleService() {
+    Path projectRoot = multiTestProject.getProjectRoot();
+    Path simpleServiceRoot = projectRoot.resolve("simple-service");
+    verifyFiles(
+        multiTestProject,
+        ImmutableList.of(
+            projectRoot.resolve("build.gradle"),
+            simpleServiceRoot.resolve("build.gradle"),
+            simpleServiceRoot.resolve("src/main/java"),
+            simpleServiceRoot.resolve("src/main/resources"),
+            simpleServiceRoot.resolve("src/main/jib")));
+  }
+
+  @Test
+  public void testFilesTask_multiProjectComplexService() {
+    Path projectRoot = multiTestProject.getProjectRoot();
+    Path complexServiceRoot = projectRoot.resolve("complex-service");
+    Path libRoot = projectRoot.resolve("lib");
+
+    verifyFiles(
+        multiTestProject,
+        ImmutableList.of(
+            projectRoot.resolve("build.gradle"),
+            libRoot.resolve("build.gradle"),
+            libRoot.resolve("src/main/java"),
+            libRoot.resolve("src/main/resources"),
+            complexServiceRoot.resolve("build.gradle"),
+            complexServiceRoot.resolve("src/main/java"),
+            complexServiceRoot.resolve("src/main/resources1"),
+            complexServiceRoot.resolve("src/main/resources2"),
+            complexServiceRoot.resolve("src/main/other-jib"),
+            Paths.get(System.getProperty("user.home"))
+                .resolve(
+                    ".m2/repository/com/google/guava/guava/HEAD-jre-SNAPSHOT/guava-HEAD-jre-SNAPSHOT.jar")));
+  }
+
+  private static void verifyFiles(TestProject project, List<Path> files) {
+    BuildResult buildResult = project.build(JibPlugin.FILES_TASK_NAME, "-q");
+    BuildTask jibTask = buildResult.task(":" + JibPlugin.FILES_TASK_NAME);
+    Assert.assertNotNull(jibTask);
+    Assert.assertEquals(TaskOutcome.SUCCESS, jibTask.getOutcome());
+
+    List<String> expectedResult =
+        files.stream().map(Path::toAbsolutePath).map(Path::toString).collect(Collectors.toList());
+    Assert.assertEquals(
+        expectedResult,
+        Splitter.on(System.lineSeparator())
+            .omitEmptyStrings()
+            .splitToList(buildResult.getOutput()));
+  }
+}
---
+++ jib-gradle-plugin/src/main/java/com/google/cloud/tools/jib/gradle/FilesTask.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.tools.jib.gradle;
+
+import com.google.common.base.Preconditions;
+import java.io.File;
+import javax.annotation.Nullable;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.TaskAction;
+
+/**
+ * Print out changing source dependencies on a project.
+ *
+ * <p>Expected use: "./gradlew _jibSkaffoldFiles -q"
+ */
+public class FilesTask extends DefaultTask {
+
+  @Nullable private JibExtension jibExtension;
+
+  public FilesTask setJibExtension(JibExtension jibExtension) {
+    this.jibExtension = jibExtension;
+    return this;
+  }
+
+  @TaskAction
+  public void listFiles() {
+    Preconditions.checkNotNull(jibExtension);
+
+    Project project = getProject();
+    JavaPluginConvention javaConvention =
+        project.getConvention().getPlugin(JavaPluginConvention.class);
+    SourceSet mainSourceSet = javaConvention.getSourceSets().findByName("main");
+    if (mainSourceSet == null) {
+      return;
+    }
+
+    // Print build.gradle
+    System.out.println(project.getBuildFile());
+
+    // Print sources
+    mainSourceSet.getAllJava().getSrcDirs().forEach(System.out::println);
+
+    // Print resources
+    mainSourceSet.getResources().getSrcDirs().forEach(System.out::println);
+
+    // Print extra layer
+    if (project.getPlugins().hasPlugin(JibPlugin.class)) {
+      System.out.println(jibExtension.getExtraDirectoryPath());
+    }
+
+    // Print dependencies
+    for (File dependency : project.getConfigurations().getByName("compile")) {
+      System.out.println(dependency.toPath().toAbsolutePath());
+    }
+  }
+}
