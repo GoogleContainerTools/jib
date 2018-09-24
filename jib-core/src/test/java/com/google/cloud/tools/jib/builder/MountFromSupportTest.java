ff9d90cc08c46c1ef72fb58217f86e89c91af4ec
Brian de Alwis <bsd@mt.ca>
2018-09-24 15:31:09 -0400

    Add missing files

---
+++ jib-core/src/test/java/com/google/cloud/tools/jib/builder/MountFromSupportTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.jib.builder;
+
+import com.google.cloud.tools.jib.JibLogger;
+import com.google.cloud.tools.jib.configuration.BuildConfiguration;
+import com.google.cloud.tools.jib.configuration.ImageConfiguration;
+import com.google.cloud.tools.jib.image.ImageReference;
+import com.google.cloud.tools.jib.image.InvalidImageReferenceException;
+import java.util.Optional;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/** Tests for {@link MountFromSupport}. */
+@RunWith(MockitoJUnitRunner.class)
+public class MountFromSupportTest {
+
+  @Mock JibLogger logger;
+
+  @Test
+  public void testSameRegistry() throws InvalidImageReferenceException {
+    BuildConfiguration buildConfiguration =
+        makeBuildConfiguration("localhost/source", "localhost/destination");
+    Optional<String> result = MountFromSupport.getMountFrom(buildConfiguration);
+    Assert.assertTrue(result.isPresent());
+    Assert.assertEquals("source", result.get());
+  }
+
+  @Test
+  public void testDifferentRegistry() throws InvalidImageReferenceException {
+    BuildConfiguration buildConfiguration =
+        makeBuildConfiguration("localhost/base", "gcr.io/target");
+    Optional<String> result = MountFromSupport.getMountFrom(buildConfiguration);
+    Assert.assertFalse(result.isPresent());
+  }
+
+  private BuildConfiguration makeBuildConfiguration(
+      String baseImageReference, String targetImageReference)
+      throws InvalidImageReferenceException {
+    ImageConfiguration baseConfiguration =
+        ImageConfiguration.builder(ImageReference.parse(baseImageReference)).build();
+    ImageConfiguration targetConfiguration =
+        ImageConfiguration.builder(ImageReference.parse(targetImageReference)).build();
+    return BuildConfiguration.builder(logger)
+        .setBaseImageConfiguration(baseConfiguration)
+        .setTargetImageConfiguration(targetConfiguration)
+        .build();
+  }
+}
