e27acd28f0002e431745c2e1876774c66fd7cf8f
Tad Cordle <tcordle@google.com>
2018-09-28 14:48:35 -0400

    Fix docker error output not being captured

--- jib-core/src/test/java/com/google/cloud/tools/jib/docker/DockerClientTest.java
+++ jib-core/src/test/java/com/google/cloud/tools/jib/docker/DockerClientTest.java
@@ -156,13 +156,15 @@ public class DockerClientTest {
     Mockito.when(mockProcess.getOutputStream()).thenReturn(ByteStreams.nullOutputStream());
     Mockito.when(mockProcess.getInputStream())
         .thenReturn(new ByteArrayInputStream("ignored".getBytes(StandardCharsets.UTF_8)));
+    Mockito.when(mockProcess.getErrorStream())
+        .thenReturn(new ByteArrayInputStream("error".getBytes(StandardCharsets.UTF_8)));
 
     try {
       testDockerClient.load(Blobs.from("jib"));
       Assert.fail("Process should have failed");
 
     } catch (IOException ex) {
-      Assert.assertEquals("'docker load' command failed with output: ignored", ex.getMessage());
+      Assert.assertEquals("'docker load' command failed with output: error", ex.getMessage());
     }
   }
 
