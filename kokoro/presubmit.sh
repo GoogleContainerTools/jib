625bb3aab14422e836d2029336cfe8b3ad2b32f8
Qingyang Chen <qingyangc@google.com>
2018-10-03 17:27:09 -0400

    fixing test

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -12,6 +12,9 @@ docker kill $(docker ps --all --quiet) || true
 
 cd github/jib
 
+# Docker is trying to use locked keychain?
+security unlock-keychain
+
 (cd jib-core; ./gradlew clean build integrationTest --info --stacktrace)
 (cd jib-plugins-common; ./gradlew clean build --info --stacktrace)
 (cd jib-maven-plugin; ./mvnw clean install -B -U -X)
