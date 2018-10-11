afee688afc33d40f79a594464b376aa712aa1916
Martin Bonato <martin.bonato@prime-sign.com>
2018-10-11 23:32:22 +0200

    https://github.com/GoogleContainerTools/jib/pull/1081#discussion_r224553137

--- jib-core/src/main/java/com/google/cloud/tools/jib/image/ReproducibleLayerBuilder.java
+++ jib-core/src/main/java/com/google/cloud/tools/jib/image/ReproducibleLayerBuilder.java
@@ -113,7 +113,7 @@ public class ReproducibleLayerBuilder {
     TarStreamBuilder tarStreamBuilder = new TarStreamBuilder();
     for (TarArchiveEntry entry : sortedFilesystemEntries) {
       // Strips out all non-reproducible elements from tar archive entries.
-      entry.setModTime(1);
+      entry.setModTime(1000);
       entry.setGroupId(0);
       entry.setUserId(0);
       entry.setUserName("");
