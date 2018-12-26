b264870b04263dfe30a12b7c255d756332cee12b
Chanseok Oh <chanseok@google.com>
2018-12-26 15:49:11 -0500

    Build with JDK 11

--- jib-core/src/test/java/com/google/cloud/tools/jib/hash/CountingDigestOutputStreamTest.java
+++ jib-core/src/test/java/com/google/cloud/tools/jib/hash/CountingDigestOutputStreamTest.java
@@ -18,6 +18,7 @@ package com.google.cloud.tools.jib.hash;
 
 import com.google.cloud.tools.jib.blob.BlobDescriptor;
 import com.google.cloud.tools.jib.image.DescriptorDigest;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.ByteStreams;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -26,36 +27,25 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.DigestException;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 /** Tests for {@link CountingDigestOutputStream}. */
 public class CountingDigestOutputStreamTest {
 
-  private Map<String, String> knownSha256Hashes;
-
-  @Before
-  public void setUp() {
-    knownSha256Hashes =
-        Collections.unmodifiableMap(
-            new HashMap<String, String>() {
-              {
-                put(
-                    "crepecake",
-                    "52a9e4d4ba4333ce593707f98564fee1e6d898db0d3602408c0b2a6a424d357c");
-                put("12345", "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5");
-                put("", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
-              }
-            });
-  }
+  private final Map<String, String> KNOWN_SHA256_HASHES =
+      ImmutableMap.of(
+          "crepecake",
+          "52a9e4d4ba4333ce593707f98564fee1e6d898db0d3602408c0b2a6a424d357c",
+          "12345",
+          "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5",
+          "",
+          "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
 
   @Test
   public void test_smokeTest() throws IOException, DigestException {
-    for (Map.Entry<String, String> knownHash : knownSha256Hashes.entrySet()) {
+    for (Map.Entry<String, String> knownHash : KNOWN_SHA256_HASHES.entrySet()) {
       String toHash = knownHash.getKey();
       String expectedHash = knownHash.getValue();
 
