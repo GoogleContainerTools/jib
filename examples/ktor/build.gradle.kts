f7844b7987960d9420f1c460a5fd86f31a889edc
Chanseok Oh <chanseok@google.com>
2019-01-17 19:04:24 -0500

    Update versions to 1.0.0 (#1433)

--- examples/ktor/build.gradle.kts
+++ examples/ktor/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
     kotlin("jvm") version "1.3.10"
-    id("com.google.cloud.tools.jib") version "1.0.0-rc2"
+    id("com.google.cloud.tools.jib") version "1.0.0"
 }
 
 group = "example"
