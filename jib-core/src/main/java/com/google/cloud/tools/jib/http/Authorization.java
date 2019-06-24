f4e2d7ce64fa0d5cf4563c6f7338e4e27a3db531
Brian de Alwis <bsd@mt.ca>
2019-06-24 15:44:45 -0400

    Move the docker-token related checks into RegistryClient

--- jib-core/src/main/java/com/google/cloud/tools/jib/http/Authorization.java
+++ jib-core/src/main/java/com/google/cloud/tools/jib/http/Authorization.java
@@ -16,19 +16,9 @@
 
 package com.google.cloud.tools.jib.http;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.google.api.client.util.Base64;
-import com.google.cloud.tools.jib.json.JsonTemplate;
-import com.google.cloud.tools.jib.json.JsonTemplateMapper;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.ImmutableSetMultimap;
-import com.google.common.collect.Multimap;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.Stream;
-import javax.annotation.Nullable;
 
 /**
  * Holds the credentials for an HTTP {@code Authorization} header.
@@ -47,7 +37,7 @@ public class Authorization {
   public static Authorization fromBasicCredentials(String username, String secret) {
     String credentials = username + ":" + secret;
     String token = Base64.encodeBase64String(credentials.getBytes(StandardCharsets.UTF_8));
-    return new Authorization("Basic", token, null);
+    return new Authorization("Basic", token);
   }
 
   /**
@@ -55,7 +45,7 @@ public class Authorization {
    * @return an {@link Authorization} with a base64-encoded {@code username:password} string
    */
   public static Authorization fromBasicToken(String token) {
-    return new Authorization("Basic", token, null);
+    return new Authorization("Basic", token);
   }
 
   /**
@@ -63,75 +53,15 @@ public class Authorization {
    * @return an {@link Authorization} with a {@code Bearer} token
    */
   public static Authorization fromBearerToken(String token) {
-    return new Authorization("Bearer", token, decodeTokenRepositoryGrants(token));
-  }
-
-  /**
-   * Decode the <a href="https://docs.docker.com/registry/spec/auth/jwt/">Docker Registry v2 Bearer
-   * Token</a> to list the granted repositories with their levels of access.
-   *
-   * @param token a Docker Registry Bearer Token
-   * @return a mapping of repository to granted access scopes, or {@code null} if the token is not a
-   *     Docker Registry Bearer Token
-   */
-  @VisibleForTesting
-  @Nullable
-  static Multimap<String, String> decodeTokenRepositoryGrants(String token) {
-    // Docker Registry Bearer Tokens are based on JWT.  A valid JWT is a set of 3 base64-encoded
-    // parts (header, payload, signature), collated with a ".".  The header and payload are
-    // JSON objects.
-    String[] jwtParts = token.split("\\.", -1);
-    byte[] payloadData;
-    if (jwtParts.length != 3 || (payloadData = Base64.decodeBase64(jwtParts[1])) == null) {
-      return null;
-    }
-
-    // The payload looks like:
-    // {
-    //   "access":[{"type":"repository","name":"repository/name","actions":["pull"]}],
-    //   "aud":"registry.docker.io",
-    //   "iss":"auth.docker.io",
-    //   "exp":999,
-    //   "iat":999,
-    //   "jti":"zzzz",
-    //   "nbf":999,
-    //   "sub":"e3ae001d-xxx"
-    // }
-    //
-    try {
-      TokenPayloadTemplate payload =
-          JsonTemplateMapper.readJson(payloadData, TokenPayloadTemplate.class);
-      if (payload.access == null) {
-        return null;
-      }
-      return payload
-          .access
-          .stream()
-          .filter(claim -> "repository".equals(claim.type))
-          .collect(
-              ImmutableSetMultimap.<AccessClaim, String, String>flatteningToImmutableSetMultimap(
-                  claim -> claim.name,
-                  claim -> claim.actions == null ? Stream.empty() : claim.actions.stream()));
-    } catch (IOException ex) {
-      return null;
-    }
+    return new Authorization("Bearer", token);
   }
 
   private final String scheme;
   private final String token;
 
-  /**
-   * If token is a Docker Registry Bearer Token, then {@link #repositoryGrants} will contain a map
-   * of repository to the access grant information extracted from the token. Otherwise, it must be
-   * {@code null}, indicating that access to all repositories are permitted.
-   */
-  @Nullable private final Multimap<String, String> repositoryGrants;
-
-  private Authorization(
-      String scheme, String token, @Nullable Multimap<String, String> repositoryGrants) {
+  private Authorization(String scheme, String token) {
     this.scheme = scheme;
     this.token = token;
-    this.repositoryGrants = repositoryGrants;
   }
 
   public String getScheme() {
@@ -163,41 +93,5 @@ public class Authorization {
   @Override
   public int hashCode() {
     return Objects.hash(scheme, token);
-  }
-
-  /**
-   * Check if this authorization allows accessing the specified repository.
-   *
-   * @param repository repository in question
-   * @param access the access scope ("push" or "pull")
-   * @return true if the repository was covered
-   */
-  public boolean canAccess(String repository, String access) {
-    // if null then we assume that all repositories are granted
-    return repositoryGrants == null || repositoryGrants.containsEntry(repository, access);
-  }
-
-  /**
-   * A simple class to represent a Docker Registry Bearer Token payload.
-   *
-   * <pre>
-   * {"access":[{"type": "repository","name": "library/openjdk","actions":["push","pull"]}]}
-   * </pre>
-   */
-  @JsonIgnoreProperties(ignoreUnknown = true)
-  private static class TokenPayloadTemplate implements JsonTemplate {
-    @Nullable private List<AccessClaim> access;
-  }
-
-  /**
-   * Represents an access claim for a repository in a Docker Registry Bearer Token payload.
-   *
-   * <pre>{"type": "repository","name": "library/openjdk","actions":["push","pull"]}</pre>
-   */
-  @JsonIgnoreProperties(ignoreUnknown = true)
-  private static class AccessClaim implements JsonTemplate {
-    @Nullable private String type;
-    @Nullable private String name;
-    @Nullable private List<String> actions;
   }
 }
