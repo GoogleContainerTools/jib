096dfc47f43b34c581aeb8eef85c4a4a5cd821bb
Appu Goundan <appu@google.com>
2019-10-10 18:11:53 -0400

    Add maven syncmap output - with support in plugins-common

---
+++ jib-maven-plugin/src/main/java/com/google/cloud/tools/jib/maven/skaffold/SyncMapMojo.java
@@ -0,0 +1,63 @@
+package com.google.cloud.tools.jib.maven.skaffold;
+
+import com.google.cloud.tools.jib.filesystem.TempDirectoryProvider;
+import com.google.cloud.tools.jib.maven.JibPluginConfiguration;
+import com.google.cloud.tools.jib.maven.MavenProjectProperties;
+import com.google.cloud.tools.jib.maven.MavenRawConfiguration;
+import com.google.cloud.tools.jib.maven.MojoCommon;
+import com.google.cloud.tools.jib.plugins.common.ContainerizingMode;
+import com.google.cloud.tools.jib.plugins.common.InvalidContainerizingModeException;
+import com.google.cloud.tools.jib.plugins.common.PluginConfigurationProcessor;
+import com.google.common.annotations.VisibleForTesting;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(
+    name = SyncMapMojo.GOAL_NAME,
+    requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME)
+public class SyncMapMojo extends JibPluginConfiguration {
+
+  @VisibleForTesting static final String GOAL_NAME = "_skaffold-sync-map";
+
+  @Override
+  public void execute() throws MojoExecutionException {
+    checkJibVersion();
+    if (MojoCommon.shouldSkipJibExecution(this)) {
+      return;
+    }
+
+    // add check that means this is only for jars
+    if (!"jar".equals(getProject().getPackaging())) {
+      throw new MojoExecutionException("Sync is currently only available for jar style projects");
+    }
+    // add check for exploded containerization
+    try {
+      if (!ContainerizingMode.EXPLODED.equals(ContainerizingMode.from(getContainerizingMode()))) {
+        throw new MojoExecutionException("Sync is only available in exploded mode");
+      }
+    } catch (InvalidContainerizingModeException ex) {
+      throw new MojoExecutionException("Invalid containerizing mode", ex);
+    }
+
+    try (TempDirectoryProvider tempDirectoryProvider = new TempDirectoryProvider()) {
+      MavenProjectProperties projectProperties =
+          MavenProjectProperties.getForProject(
+              getProject(), getSession(), getLog(), tempDirectoryProvider);
+
+      MavenRawConfiguration configuration = new MavenRawConfiguration(this);
+
+      try {
+        // should I print something out here to ID it? sync map might require that "package" is
+        // also run next to in the multimodule case to identify submodule dependencies
+        // print BEGIN JIB SYNC MAP
+        String syncMapJson =
+            PluginConfigurationProcessor.getSkaffoldSyncMap(configuration, projectProperties);
+        System.out.println(syncMapJson);
+        // print END JIB SYNC MAP
+      } catch (Exception e) {
+        throw new MojoExecutionException("Failed to generate Jib file map", e);
+      }
+    }
+  }
+}
---
+++ jib-maven-plugin/src/test/java/com/google/cloud/tools/jib/maven/skaffold/SyncMapMojoTest.java
@@ -0,0 +1,106 @@
+package com.google.cloud.tools.jib.maven.skaffold;
+
+import com.google.cloud.tools.jib.api.AbsoluteUnixPath;
+import com.google.cloud.tools.jib.maven.TestProject;
+import com.google.cloud.tools.jib.plugins.common.SkaffoldSyncMapTemplate;
+import com.google.cloud.tools.jib.plugins.common.SkaffoldSyncMapTemplate.FileTemplate;
+import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import org.apache.maven.it.VerificationException;
+import org.apache.maven.it.Verifier;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+/** Tests for {@link SyncMapMojo} */
+public class SyncMapMojoTest {
+
+  @ClassRule public static final TestProject simpleTestProject = new TestProject("simple");
+
+  @ClassRule public static final TestProject multiTestProject = new TestProject("multi");
+
+  private static String getLines(Path projectRoot, String module)
+      throws VerificationException, IOException {
+    Verifier verifier = new Verifier(projectRoot.toString());
+    verifier.setAutoclean(false);
+    verifier.addCliOption("-q");
+    if (!Strings.isNullOrEmpty(module)) {
+      verifier.addCliOption("-pl");
+      verifier.addCliOption(module);
+      verifier.addCliOption("-am");
+      verifier.addCliOption("-Djib.containerize=com.jib.test:" + module);
+    }
+    verifier.addCliOption("-DskipTests");
+    verifier.executeGoals(ImmutableList.of("package", "jib:" + SyncMapMojo.GOAL_NAME));
+
+    Path logFile = Paths.get(verifier.getBasedir()).resolve(verifier.getLogFileName());
+    return new String(Files.readAllBytes(logFile), Charsets.UTF_8);
+  }
+
+  private static void assertFilePaths(Path src, AbsoluteUnixPath dest, FileTemplate template) {
+    Assert.assertEquals(src.toString(), template.getSrc());
+    Assert.assertEquals(dest.toString(), template.getDest());
+  }
+
+  @Test
+  public void testSyncMapMojo_simpleTestProjectOutput() throws IOException, VerificationException {
+    Path projectRoot = simpleTestProject.getProjectRoot();
+    String JSON = getLines(projectRoot, null);
+    SkaffoldSyncMapTemplate parsed = SkaffoldSyncMapTemplate.from(JSON);
+
+    List<FileTemplate> generated = parsed.getGenerated();
+    Assert.assertEquals(2, generated.size());
+    assertFilePaths(
+        projectRoot.resolve("target/classes/world"),
+        AbsoluteUnixPath.get("/app/resources/world"),
+        generated.get(0));
+    assertFilePaths(
+        projectRoot.resolve("target/classes/com/test/HelloWorld.class"),
+        AbsoluteUnixPath.get("/app/classes/com/test/HelloWorld.class"),
+        generated.get(1));
+
+    List<FileTemplate> direct = parsed.getDirect();
+    Assert.assertEquals(2, direct.size());
+    assertFilePaths(
+        projectRoot.resolve("src/main/jib-custom/bar/cat"),
+        AbsoluteUnixPath.get("/bar/cat"),
+        direct.get(0));
+    assertFilePaths(
+        projectRoot.resolve("src/main/jib-custom/foo"),
+        AbsoluteUnixPath.get("/foo"),
+        direct.get(1));
+  }
+
+  @Test
+  public void testSyncMapMojo_multiProjectOutput() throws IOException, VerificationException {
+    Path projectRoot = multiTestProject.getProjectRoot();
+    Path m2 = Paths.get(System.getProperty("user.home")).resolve(".m2").resolve("repository");
+    String JSON = getLines(projectRoot, "complex-service");
+    SkaffoldSyncMapTemplate parsed = SkaffoldSyncMapTemplate.from(JSON);
+
+    List<FileTemplate> generated = parsed.getGenerated();
+    Assert.assertEquals(2, generated.size());
+    assertFilePaths(
+        projectRoot.resolve("lib/target/lib-1.0.0.TEST-SNAPSHOT.jar"),
+        AbsoluteUnixPath.get("/app/libs/lib-1.0.0.TEST-SNAPSHOT.jar"),
+        generated.get(0));
+    assertFilePaths(
+        projectRoot.resolve("complex-service/target/classes/com/test/HelloWorld.class"),
+        AbsoluteUnixPath.get("/app/classes/com/test/HelloWorld.class"),
+        generated.get(1));
+
+    List<FileTemplate> direct = parsed.getDirect();
+    Assert.assertEquals(1, direct.size());
+    assertFilePaths(
+        m2.resolve(
+            "com/google/cloud/tools/tiny-test-lib/0.0.1-SNAPSHOT/tiny-test-lib-0.0.1-SNAPSHOT.jar"),
+        AbsoluteUnixPath.get("/app/libs/tiny-test-lib-0.0.1-SNAPSHOT.jar"),
+        direct.get(0));
+  }
+}
