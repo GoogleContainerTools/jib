f1ce7420b8df6893498abd285b266505cd1f340e
Chanseok Oh <chanseok@google.com>
2019-10-16 12:43:08 -0400

    Explode final WAR

--- jib-maven-plugin/src/main/java/com/google/cloud/tools/jib/maven/BuildTarMojo.java
+++ jib-maven-plugin/src/main/java/com/google/cloud/tools/jib/maven/BuildTarMojo.java
@@ -18,6 +18,7 @@ package com.google.cloud.tools.jib.maven;
 
 import com.google.cloud.tools.jib.api.CacheDirectoryCreationException;
 import com.google.cloud.tools.jib.api.InvalidImageReferenceException;
+import com.google.cloud.tools.jib.filesystem.TempDirectoryProvider;
 import com.google.cloud.tools.jib.plugins.common.BuildStepsExecutionException;
 import com.google.cloud.tools.jib.plugins.common.HelpfulSuggestions;
 import com.google.cloud.tools.jib.plugins.common.IncompatibleBaseImageJavaVersionException;
@@ -75,8 +76,10 @@ public class BuildTarMojo extends JibPluginConfiguration {
     MavenSettingsProxyProvider.activateHttpAndHttpsProxies(
         getSession().getSettings(), getSettingsDecrypter());
 
+    TempDirectoryProvider tempDirectoryProvider = new TempDirectoryProvider();
     MavenProjectProperties projectProperties =
-        MavenProjectProperties.getForProject(getProject(), getSession(), getLog());
+        MavenProjectProperties.getForProject(
+            getProject(), getSession(), getLog(), tempDirectoryProvider);
     try {
       PluginConfigurationProcessor.createJibBuildRunnerForTarImage(
               new MavenRawConfiguration(this),
@@ -137,6 +140,7 @@ public class BuildTarMojo extends JibPluginConfiguration {
       throw new MojoExecutionException(ex.getMessage(), ex.getCause());
 
     } finally {
+      tempDirectoryProvider.close();
       projectProperties.waitForLoggingThread();
       getLog().info("");
     }
