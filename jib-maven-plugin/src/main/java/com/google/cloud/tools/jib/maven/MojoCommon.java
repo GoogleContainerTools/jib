73b77ba4cada4959ee96897c042cb0abcef36c21
Appu Goundan <appu@google.com>
2019-10-25 15:28:40 -0400

    Move skip execution logic to MojoCommon

--- jib-maven-plugin/src/main/java/com/google/cloud/tools/jib/maven/MojoCommon.java
+++ jib-maven-plugin/src/main/java/com/google/cloud/tools/jib/maven/MojoCommon.java
@@ -19,6 +19,7 @@ package com.google.cloud.tools.jib.maven;
 import com.google.cloud.tools.jib.api.AbsoluteUnixPath;
 import com.google.cloud.tools.jib.api.FilePermissions;
 import com.google.cloud.tools.jib.maven.JibPluginConfiguration.PermissionConfiguration;
+import com.google.cloud.tools.jib.plugins.common.PropertyNames;
 import com.google.cloud.tools.jib.plugins.common.VersionChecker;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
@@ -30,6 +31,7 @@ import java.util.Map;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 
 /** Collection of common methods to share between Maven goals. */
@@ -120,6 +122,25 @@ public class MojoCommon {
               actualVersion, acceptableVersionSpec);
       throw new MojoExecutionException(failure);
     }
+  }
+
+  public static boolean shouldSkipJibExecution(JibPluginConfiguration jibPluginConfiguration) {
+    Log log = jibPluginConfiguration.getLog();
+    if (jibPluginConfiguration.isSkipped()) {
+      log.info("Skipping containerization because jib-maven-plugin: skip = true");
+      return true;
+    } else if (!jibPluginConfiguration.isContainerizable()) {
+      log.info(
+          "Skipping containerization of this module (not specified in "
+              + PropertyNames.CONTAINERIZE
+              + ")");
+      return true;
+    }
+    if ("pom".equals(jibPluginConfiguration.getProject().getPackaging())) {
+      log.info("Skipping containerization because packaging is 'pom'...");
+      return true;
+    }
+    return false;
   }
 
   private MojoCommon() {}
