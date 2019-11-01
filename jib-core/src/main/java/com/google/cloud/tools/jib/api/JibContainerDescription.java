e12df5c3e10878d349146ddcb97b909ab0b71e68
Appu Goundan <appu@google.com>
2019-11-01 12:07:24 -0400

    copyright, javadoc, git is weird files

--- jib-core/src/main/java/com/google/cloud/tools/jib/api/JibContainerDescription.java
+++ jib-core/src/main/java/com/google/cloud/tools/jib/api/JibContainerDescription.java
@@ -1,16 +1,33 @@
+/*
+ * Copyright 2019 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.cloud.tools.jib.api;
 
 import com.google.cloud.tools.jib.configuration.ContainerConfiguration;
+import com.google.common.annotations.Beta;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 
 /**
  * A class containing the representation of the contents of a container. Currently only exposes
- * "layers", but can be extended to expose {@link ContainerConfiguration} or other informational
- * classes.
+ * "layers", but can be extended to expose {@link ContainerConfiguration}, {@link ImageReference}
+ * of the base image, or other informational classes.
  *
  * <p>This class is immutable and thread-safe.
  */
+@Beta
 public class JibContainerDescription {
 
   private final ImmutableList<LayerConfiguration> layers;
@@ -19,7 +36,10 @@ public class JibContainerDescription {
     this.layers = ImmutableList.copyOf(layers);
   }
 
-  /** Returns a list of "user configured" layers, does *not* include base layer information. */
+  /**
+   * Returns a list of "user configured" layers, does *not* include base layer information.
+   * @return An {@link ImmutableList} of {@link LayerConfiguration}s
+   */
   public List<LayerConfiguration> getLayers() {
     return layers;
   }
---
+++ jib-plugins-common/README.md
@@ -0,0 +1,1 @@
+Common code for Jib plugins. NOT intended for use outside of `jib-maven-plugin` or `jib-gradle-plugin` in its current state.
