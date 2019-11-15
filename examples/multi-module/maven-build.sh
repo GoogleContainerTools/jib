65a56971ec6ab457409d06c12c287e0a1541da4e
Appu <appu@google.com>
2019-11-15 17:19:17 -0500

    Update multimodule example with module dependency (#2155)

--- examples/multi-module/maven-build.sh
+++ examples/multi-module/maven-build.sh
@@ -3,4 +3,9 @@
 set -ex
 
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
-./mvnw compile jib:build
+# if there are no intermodule dependencies, compile is enough to complete a jib build.
+./mvnw compile jib:build -pl hello-service
+
+# multi module builds with dependencies on other modules in the build require that the
+# "package" phase is executed as part of the build to correctly include module dependencies.
+./mvnw package jib:build -pl name-service -am
