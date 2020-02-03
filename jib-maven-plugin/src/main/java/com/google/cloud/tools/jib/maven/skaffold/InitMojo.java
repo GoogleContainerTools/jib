4814932ba0940a0dbeacca5fc6f334a31d431d17
tad.cordle@gmail.com <tcordle@google.com>
2020-02-03 15:53:43 -0500

    Fix incorrect skaffold init output in multi-module projects

--- jib-maven-plugin/src/main/java/com/google/cloud/tools/jib/maven/skaffold/InitMojo.java
+++ jib-maven-plugin/src/main/java/com/google/cloud/tools/jib/maven/skaffold/InitMojo.java
@@ -20,6 +20,8 @@ import com.google.cloud.tools.jib.maven.JibPluginConfiguration;
 import com.google.cloud.tools.jib.plugins.common.SkaffoldInitOutput;
 import com.google.common.annotations.VisibleForTesting;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
@@ -39,15 +41,42 @@ public class InitMojo extends JibPluginConfiguration {
   public void execute() throws MojoExecutionException {
     checkJibVersion();
     MavenProject project = getProject();
-    // Ignore parent projects
-    if (project.getModules().size() > 0) {
+    MavenProject topLevelProject = getSession().getTopLevelProject();
+    if (!project.equals(topLevelProject)) {
       return;
     }
 
+    if (isParentProject(project)) {
+      Set<MavenProject> leafProjects = collectLeafProjects(project);
+      for (MavenProject leafProject : leafProjects) {
+        print(leafProject, true);
+      }
+    } else {
+      print(project, false);
+    }
+  }
+
+  private Set<MavenProject> collectLeafProjects(MavenProject project) {
+    Set<MavenProject> projects = new HashSet<>();
+    for (MavenProject p : project.getCollectedProjects()) {
+      if (isParentProject(p)) {
+        projects.addAll(collectLeafProjects(p));
+      } else {
+        projects.add(p);
+      }
+    }
+    return projects;
+  }
+
+  private boolean isParentProject(MavenProject project) {
+    return project.getPackaging().equals("pom") && project.getModules().size() > 0;
+  }
+
+  private void print(MavenProject project, boolean setProject) throws MojoExecutionException {
     SkaffoldInitOutput skaffoldInitOutput = new SkaffoldInitOutput();
     skaffoldInitOutput.setImage(getTargetImage());
-    if (project.getParent() != null && project.getParent().getFile() != null) {
-      skaffoldInitOutput.setProject(project.getArtifactId());
+    if (setProject) {
+      skaffoldInitOutput.setProject(project.getGroupId() + ":" + project.getArtifactId());
     }
     System.out.println();
     System.out.println("BEGIN JIB JSON");
