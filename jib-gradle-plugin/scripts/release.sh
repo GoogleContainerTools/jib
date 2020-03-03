6cd266cd1386498bab9e165f5593b2dceb98a470
Chanseok Oh <chanseok@google.com>
2020-03-03 15:22:51 -0500

    Set environment variable

--- jib-gradle-plugin/scripts/release.sh
+++ jib-gradle-plugin/scripts/release.sh
@@ -3,12 +3,14 @@
 set -o errexit
 set -o xtrace
 
-readonly GRADLE_PUBLISH_KEY=$(cat "${KOKORO_KEYSTORE_DIR}/72743_gradle_publish_key")
-readonly GRADLE_PUBLISH_SECRET=$(cat "${KOKORO_KEYSTORE_DIR}/72743_gradle_publish_secret")
+readonly PUBLISH_KEY=$(cat "${KOKORO_KEYSTORE_DIR}/72743_gradle_publish_key")
+readonly PUBLISH_SECRET=$(cat "${KOKORO_KEYSTORE_DIR}/72743_gradle_publish_secret")
+
+export readonly JIB_INTEGRATION_TESTING_PROJECT=jib-integration-testing
 
 cd github/jib
 
 ./gradlew :jib-gradle-plugin:publishPlugins \
-  -Pgradle.publish.key="${GRADLE_PUBLISH_KEY}" \
-  -Pgradle.publish.secret="${GRADLE_PUBLISH_SECRET}" \
+  -Pgradle.publish.key="${PUBLISH_KEY}" \
+  -Pgradle.publish.secret="${PUBLISH_SECRET}" \
   --info --stacktrace
