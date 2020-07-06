2a487c0bf1d04e44176fec28ede89f6c86ac7bfb
Louis Murerwa <louismurerwa@google.com>
2020-07-06 21:35:03 +0000

    Adding Platforms Specifications for jib-maven-plugin

--- jib-maven-plugin/src/test/java/com/google/cloud/tools/jib/maven/JibPluginConfigurationTest.java
+++ jib-maven-plugin/src/test/java/com/google/cloud/tools/jib/maven/JibPluginConfigurationTest.java
@@ -69,6 +69,8 @@ public class JibPluginConfigurationTest {
 
   @Test
   public void testDefaults() {
+    Assert.assertEquals("linux", testPluginConfiguration.getPlatforms().get(0).getOs());
+    Assert.assertEquals("amd64", testPluginConfiguration.getPlatforms().get(0).getArchitecture());
     Assert.assertEquals("", testPluginConfiguration.getAppRoot());
     Assert.assertNull(testPluginConfiguration.getWorkingDirectory());
     Assert.assertTrue(testPluginConfiguration.getExtraClasspath().isEmpty());
@@ -181,6 +183,7 @@ public class JibPluginConfigurationTest {
 
   @Test
   public void testPomProperties() {
+
     project.getProperties().setProperty("jib.from.image", "fromImage");
     Assert.assertEquals("fromImage", testPluginConfiguration.getBaseImage());
     project.getProperties().setProperty("jib.from.credHelper", "credHelper");
