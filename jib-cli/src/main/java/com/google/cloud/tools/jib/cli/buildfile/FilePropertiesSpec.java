19eb9ced8a1a7d340b92f63bed7c6d953f3105be
Appu Goundan <appu@google.com>
2020-07-08 13:14:12 -0400

    style

--- jib-cli/src/main/java/com/google/cloud/tools/jib/cli/buildfile/FilePropertiesSpec.java
+++ jib-cli/src/main/java/com/google/cloud/tools/jib/cli/buildfile/FilePropertiesSpec.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.google.cloud.tools.jib.cli.buildfile;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -10,7 +26,7 @@ import javax.annotation.Nullable;
 /**
  * A yaml block for specifying file properties (used in conjunction with layers).
  *
- * <p>Example use of this yaml snippet
+ * <p>Example use of this yaml snippet.
  *
  * <pre>{@code
  * filePermissions: 644
@@ -27,6 +43,7 @@ public class FilePropertiesSpec {
   @Nullable private String group;
   @Nullable private Instant timestamp;
 
+  /** Constructor for use by jackson to populate this object. */
   @JsonCreator
   public FilePropertiesSpec(
       @JsonProperty("filePermissions") String filePermissions,
