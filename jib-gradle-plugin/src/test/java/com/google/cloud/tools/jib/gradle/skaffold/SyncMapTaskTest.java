c8ccb15bef437cafba20f75f05805d2ef91e64b1
Mridula Peddada <mpeddada@google.com>
2020-09-13 22:15:57 -0400

    more fixes

--- jib-gradle-plugin/src/test/java/com/google/cloud/tools/jib/gradle/skaffold/SyncMapTaskTest.java
+++ jib-gradle-plugin/src/test/java/com/google/cloud/tools/jib/gradle/skaffold/SyncMapTaskTest.java
@@ -61,7 +61,7 @@ public class SyncMapTaskTest {
     List<String> buildParams = new ArrayList<>();
     buildParams.add(taskName);
     buildParams.add("-q");
-    buildParams.add("-D_TARGET_IMAGE");
+    buildParams.add("-D_TARGET_IMAGE=ignored");
     buildParams.add("--stacktrace");
     if (params != null) {
       buildParams.addAll(params);
