GoogleContainerTools/jib

commit faff19bd59737e10db4425e47b7e6c9904e83833

Use Gradle Property and Provider to enable lazy evaluation for jib.to.image and jib.to.tags (#2739)
* Using gradle Property and Provider to enable lazy evaluation

* adding tests and sample project

jib-gradle-plugin/src/main/java/com/google/cloud/tools/jib/gradle/TargetImageParameters.java
@@ -19,12 +19,14 @@ package com.google.cloud.tools.jib.gradle;
 import com.google.cloud.tools.jib.plugins.common.ConfigurationPropertyValidator;
 import com.google.cloud.tools.jib.plugins.common.PropertyNames;
 import com.google.common.collect.ImmutableSet;
-import java.util.Collections;
 import java.util.Set;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import org.gradle.api.Action;
 import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.Property;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.provider.SetProperty;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.Nested;
 import org.gradle.api.tasks.Optional;
@@ -34,13 +36,15 @@ public class TargetImageParameters {
 
   private final AuthParameters auth;
 
-  @Nullable private String image;
-  private Set<String> tags = Collections.emptySet();
+  private Property<String> image;
+  private SetProperty<String> tags;
   @Nullable private String credHelper;
 
   @Inject
   public TargetImageParameters(ObjectFactory objectFactory) {
     auth = objectFactory.newInstance(AuthParameters.class, "to.auth");
+    image = objectFactory.property(String.class);
+    tags = objectFactory.setProperty(String.class).empty();
   }
 
   @Input
@@ -50,11 +54,15 @@ public class TargetImageParameters {
     if (System.getProperty(PropertyNames.TO_IMAGE) != null) {
       return System.getProperty(PropertyNames.TO_IMAGE);
     }
-    return image;
+    return image.getOrNull();
   }
 
   public void setImage(String image) {
-    this.image = image;
+    this.image.set(image);
+  }
+
+  public void setImage(Provider<String> image) {
+    this.image.set(image);
   }
 
   @Input
@@ -65,11 +73,15 @@ public class TargetImageParameters {
           ConfigurationPropertyValidator.parseListProperty(
               System.getProperty(PropertyNames.TO_TAGS)));
     }
-    return tags;
+    return tags.get();
   }
 
   public void setTags(Set<String> tags) {
-    this.tags = tags;
+    this.tags.set(tags);
+  }
+
+  public void setTags(Provider<Set<String>> tags) {
+    this.tags.set(tags);
   }
 
   @Input

jib-gradle-plugin/src/test/java/com/google/cloud/tools/jib/gradle/JibPluginTest.java
@@ -57,6 +57,7 @@ public class JibPluginTest {
   }
 
   @Rule public final TemporaryFolder testProjectRoot = new TemporaryFolder();
+  @Rule public final TestProject testProject = new TestProject("lazy-evaluation");
 
   @After
   public void tearDown() {
@@ -368,6 +369,25 @@ public class JibPluginTest {
     TaskContainer tasks = project.getTasks();
     KNOWN_JIB_TASKS.forEach(
         taskName -> Assert.assertEquals(taskName, "Jib", tasks.getByPath(taskName).getGroup()));
+  }
+
+  @Test
+  public void testLazyEvalForImageAndTags() {
+    // TODO: Pass in `-Djib.console=plain` as argument for build and remove filtering for cyan
+    // coloring regex once [#2764](https://github.com/GoogleContainerTools/jib/issues/2764) is
+    // submitted.
+    try {
+      testProject.build(JibPlugin.BUILD_IMAGE_TASK_NAME);
+      Assert.fail("Expect this to fail");
+    } catch (UnexpectedBuildFailure ex) {
+      String output = ex.getBuildResult().getOutput().trim();
+      String cleanOutput = output.replace("\u001B[36m", "").replace("\u001B[0m", "");
+
+      MatcherAssert.assertThat(
+          cleanOutput,
+          CoreMatchers.containsString(
+              "Containerizing application to updated-image, updated-image:updated-tag, updated-image:tag2"));
+    }
   }
 
   private Project createProject(String... plugins) {

jib-gradle-plugin/src/test/java/com/google/cloud/tools/jib/gradle/skaffold/FilesTaskV2Test.java
@@ -58,7 +58,7 @@ public class FilesTaskV2Test {
   private static String verifyTaskSuccess(TestProject project, @Nullable String moduleName) {
     String taskName =
         ":" + (moduleName == null ? "" : moduleName + ":") + JibPlugin.SKAFFOLD_FILES_TASK_V2_NAME;
-    BuildResult buildResult = project.build(taskName, "-q");
+    BuildResult buildResult = project.build(taskName, "-q", "-D_TARGET_IMAGE=ignored");
     BuildTask jibTask = buildResult.task(taskName);
     Assert.assertNotNull(jibTask);
     Assert.assertEquals(TaskOutcome.SUCCESS, jibTask.getOutcome());

jib-gradle-plugin/src/test/java/com/google/cloud/tools/jib/gradle/skaffold/SyncMapTaskTest.java
@@ -61,6 +61,7 @@ public class SyncMapTaskTest {
     List<String> buildParams = new ArrayList<>();
     buildParams.add(taskName);
     buildParams.add("-q");
+    buildParams.add("-D_TARGET_IMAGE=ignored");
     buildParams.add("--stacktrace");
     if (params != null) {
       buildParams.addAll(params);

jib-gradle-plugin/src/test/resources/gradle/projects/lazy-evaluation/build.gradle
@@ -1,0 +1,28 @@
+plugins {
+    id 'java'
+    id 'com.google.cloud.tools.jib'
+}
+
+sourceCompatibility = 1.8
+targetCompatibility = 1.8
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile 'com.google.guava:guava:23.6-jre'
+}
+
+project.ext.value = "original"
+
+project.afterEvaluate {
+    project.ext.value = "updated"
+}
+
+jib {
+    to {
+        image = project.provider {project.ext.value + "-image"}
+        tags = project.provider {[project.ext.value + "-tag", "tag2"]}
+    }
+}

jib-gradle-plugin/src/test/resources/gradle/projects/lazy-evaluation/src/main/java/com/test/HelloWorld.java
@@ -1,0 +1,27 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class HelloWorld {
+
+  public static void main(String[] args) throws URISyntaxException, IOException {
+    System.out.println("Hello world");
+  }
+}

commit 23defe808f2e79a7c10706412cf9cb1836c4ab06

Fix to handle integration test failure caused by #2739 (#2773)
* Add setTag method that accepts a list

jib-gradle-plugin/src/main/java/com/google/cloud/tools/jib/gradle/TargetImageParameters.java
@@ -19,6 +19,7 @@ package com.google.cloud.tools.jib.gradle;
 import com.google.cloud.tools.jib.plugins.common.ConfigurationPropertyValidator;
 import com.google.cloud.tools.jib.plugins.common.PropertyNames;
 import com.google.common.collect.ImmutableSet;
+import java.util.List;
 import java.util.Set;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
@@ -74,6 +75,10 @@ public class TargetImageParameters {
               System.getProperty(PropertyNames.TO_TAGS)));
     }
     return tags.get();
+  }
+
+  public void setTags(List<String> tags) {
+    this.tags.set(tags);
   }
 
   public void setTags(Set<String> tags) {