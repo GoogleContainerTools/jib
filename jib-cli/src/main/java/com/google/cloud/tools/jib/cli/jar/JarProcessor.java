cecfd011b9592a77306b4a83e2dcb38b3c36317f
Mridula Peddada <mpeddada@google.com>
2020-09-30 15:32:58 -0400

    Implement JarProcessor to determine jar type and use Truth for tests

---
+++ jib-cli/src/main/java/com/google/cloud/tools/jib/cli/jar/JarProcessor.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.tools.jib.cli.jar;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.jar.JarFile;
+
+public class JarProcessor {
+
+  /**
+   * Jar Type.
+   *
+   * <ul>
+   *   <li>{@code REGULAR} a regular jar.
+   *   <li>{@code SPRINGBOOT} a springboot fat jar. *
+   * </ul>
+   */
+  public enum JarType {
+    REGULAR,
+    SPRINGBOOT;
+  }
+
+  /**
+   * Determines whether the jar is a spring-boot or regular jar, given a path to the jar.
+   *
+   * @param jarPath path to the jar.
+   * @return the jar type.
+   * @throws IOException if I/O error occurs when opening the file.
+   */
+  public static JarType determineJarType(Path jarPath) throws IOException {
+    JarFile jarFile = new JarFile(jarPath.toString());
+    if (jarFile.getEntry("BOOT-INF") != null) {
+      return JarType.SPRINGBOOT;
+    }
+    return JarType.REGULAR;
+  }
+}
---
+++ jib-cli/src/test/java/com/google/cloud/tools/jib/cli/jar/JarProcessorTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.tools.jib.cli.jar;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.cloud.tools.jib.cli.jar.JarProcessor.JarType;
+import com.google.common.io.Resources;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.Test;
+
+public class JarProcessorTest {
+
+  private static final String SPRINGBOOT_RESOURCE_DIR = "jar/springboot";
+  private static final String REGULAR_RESOURCE_DIR = "jar/regular";
+
+  @Test
+  public void testDetermineJarType_springBoot() throws IOException, URISyntaxException {
+    Path testDir = Paths.get(Resources.getResource(SPRINGBOOT_RESOURCE_DIR).toURI());
+    Path springBootJar = testDir.resolve("springbootJar.jar");
+    JarType jarType = JarProcessor.determineJarType(springBootJar);
+    assertThat(jarType).isEqualTo(JarType.SPRINGBOOT);
+  }
+
+  @Test
+  public void testDetermineJarType_regular() throws IOException, URISyntaxException {
+    Path testDir = Paths.get(Resources.getResource(REGULAR_RESOURCE_DIR).toURI());
+    Path regularJar = testDir.resolve("regularJar.jar");
+    JarType jarType = JarProcessor.determineJarType(regularJar);
+    assertThat(jarType).isEqualTo(JarType.REGULAR);
+  }
+}
