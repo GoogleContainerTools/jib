c300c7a52d51cb2ca6bfaa1a7d53e62a7da1944b
Mridula <66699525+mpeddada1@users.noreply.github.com>
2020-12-16 16:12:53 -0500

    Add integration tests for explode mode of spring boot jar (#2926)

--- jib-cli/src/integration-test/java/com/google/cloud/tools/jib/cli/cli2/JarCommandTest.java
+++ jib-cli/src/integration-test/java/com/google/cloud/tools/jib/cli/cli2/JarCommandTest.java
@@ -19,19 +19,42 @@ package com.google.cloud.tools.jib.cli.cli2;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.cloud.tools.jib.Command;
+import com.google.cloud.tools.jib.blob.Blobs;
 import com.google.common.io.Resources;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
+import javax.annotation.Nullable;
+import org.junit.After;
+import org.junit.ClassRule;
 import org.junit.Test;
 import picocli.CommandLine;
 
 public class JarCommandTest {
+
+  @ClassRule
+  public static final TestProject springBootProjectLayered = new TestProject("springBootLayered");
+
+  @ClassRule
+  public static final TestProject springBootProjectNonLayered =
+      new TestProject("springBootNonLayered");
+
+  @Nullable private String containerName;
+
+  @After
+  public void tearDown() throws IOException, InterruptedException {
+    if (containerName != null) {
+      new Command("docker", "stop", containerName).run();
+    }
+  }
 
   @Test
   public void testErrorLogging_fileDoesNotExist() {
@@ -63,9 +86,9 @@ public class JarCommandTest {
   }
 
   @Test
-  public void testJar_explodedMode_toDocker()
+  public void testStandardJar_explodedMode_toDocker()
       throws IOException, InterruptedException, URISyntaxException {
-    Path jarPath = Paths.get(Resources.getResource("jarTest/jarWithCp.jar").toURI());
+    Path jarPath = Paths.get(Resources.getResource("jarTest/standard/jarWithCp.jar").toURI());
     Integer exitCode =
         new CommandLine(new JibCli())
             .execute("jar", "--target", "docker://exploded-jar", jarPath.toString());
@@ -81,9 +104,9 @@ public class JarCommandTest {
   }
 
   @Test
-  public void testNoDependencyJar_explodedMode_toDocker()
+  public void testNoDependencyStandardJar_explodedMode_toDocker()
       throws IOException, InterruptedException, URISyntaxException {
-    Path jarPath = Paths.get(Resources.getResource("noDependencyJar.jar").toURI());
+    Path jarPath = Paths.get(Resources.getResource("jarTest/standard/noDependencyJar.jar").toURI());
     Integer exitCode =
         new CommandLine(new JibCli())
             .execute("jar", "--target", "docker://exploded-no-dep-jar", jarPath.toString());
@@ -99,9 +122,9 @@ public class JarCommandTest {
   }
 
   @Test
-  public void testJar_packagedMode_toDocker()
+  public void testStandardJar_packagedMode_toDocker()
       throws IOException, InterruptedException, URISyntaxException {
-    Path jarPath = Paths.get(Resources.getResource("jarTest/jarWithCp.jar").toURI());
+    Path jarPath = Paths.get(Resources.getResource("jarTest/standard/jarWithCp.jar").toURI());
     Integer exitCode =
         new CommandLine(new JibCli())
             .execute(
@@ -118,9 +141,9 @@ public class JarCommandTest {
   }
 
   @Test
-  public void testNoDependencyJar_packagedMode_toDocker()
+  public void testNoDependencyStandardJar_packagedMode_toDocker()
       throws IOException, InterruptedException, URISyntaxException {
-    Path jarPath = Paths.get(Resources.getResource("noDependencyJar.jar").toURI());
+    Path jarPath = Paths.get(Resources.getResource("jarTest/standard/noDependencyJar.jar").toURI());
     Integer exitCode =
         new CommandLine(new JibCli())
             .execute(
@@ -153,5 +176,67 @@ public class JarCommandTest {
     assertThat(stringWriter.toString())
         .contains(
             "Invalid value for option '--mode': expected one of [exploded, packaged] (case-sensitive) but was 'unknown'");
+  }
+
+  @Test
+  public void testSpringBootLayeredJar_explodedMode() throws IOException, InterruptedException {
+    springBootProjectLayered.build("clean", "bootJar");
+    Path jarParentPath = springBootProjectLayered.getProjectRoot().resolve("build").resolve("libs");
+    Path jarPath = jarParentPath.resolve("spring-boot-layered.jar");
+
+    Integer exitCode =
+        new CommandLine(new JibCli())
+            .execute("jar", "--target", "docker://spring-boot-jar-layered", jarPath.toString());
+    assertThat(exitCode).isEqualTo(0);
+
+    String output =
+        new Command("docker", "run", "--rm", "--detach", "-p8080:8080", "spring-boot-jar-layered")
+            .run();
+    containerName = output.trim();
+    try (JarFile jarFile = new JarFile(jarPath.toFile())) {
+
+      assertThat(jarFile.getEntry("BOOT-INF/layers.idx")).isNotNull();
+      assertThat(getContent(new URL("http://localhost:8080"))).isEqualTo("Hello world");
+    }
+  }
+
+  @Test
+  public void testSpringBootNonLayeredJar_explodedMode() throws IOException, InterruptedException {
+    springBootProjectNonLayered.build("clean", "bootJar");
+    Path jarParentPath =
+        springBootProjectNonLayered.getProjectRoot().resolve("build").resolve("libs");
+    Path jarPath = jarParentPath.resolve("spring-boot-nonlayered.jar");
+
+    Integer exitCode =
+        new CommandLine(new JibCli())
+            .execute("jar", "--target", "docker://spring-boot-jar", jarPath.toString());
+    assertThat(exitCode).isEqualTo(0);
+
+    String output =
+        new Command("docker", "run", "--rm", "--detach", "-p8080:8080", "spring-boot-jar").run();
+    containerName = output.trim();
+    try (JarFile jarFile = new JarFile(jarPath.toFile())) {
+
+      assertThat(jarFile.getEntry("BOOT-INF/layers.idx")).isNull();
+      assertThat(getContent(new URL("http://localhost:8080"))).isEqualTo("Hello world");
+    }
+  }
+
+  @Nullable
+  private static String getContent(URL url) throws InterruptedException {
+    for (int i = 0; i < 40; i++) {
+      Thread.sleep(500);
+      try {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+          try (InputStream in = connection.getInputStream()) {
+            return Blobs.writeToString(Blobs.from(in));
+          }
+        }
+      } catch (IOException ignored) {
+        // ignored
+      }
+    }
+    return null;
   }
 }
---
+++ jib-cli/src/integration-test/java/com/google/cloud/tools/jib/cli/cli2/TestProject.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.tools.jib.cli.cli2;
+
+import com.google.cloud.tools.jib.filesystem.DirectoryWalker;
+import com.google.common.io.Resources;
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.gradle.util.GradleVersion;
+import org.junit.rules.TemporaryFolder;
+
+public class TestProject extends TemporaryFolder implements Closeable {
+
+  private static final String PROJECTS_PATH_IN_RESOURCES = "jarTest/spring-boot/";
+
+  /** Copies test project {@code projectName} to {@code destination} folder. */
+  private static void copyProject(String projectName, Path destination)
+      throws IOException, URISyntaxException {
+    Path projectPathInResources =
+        Paths.get(Resources.getResource(PROJECTS_PATH_IN_RESOURCES + projectName).toURI());
+    new DirectoryWalker(projectPathInResources)
+        .filterRoot()
+        .walk(
+            path -> {
+              // Creates the same path in the destDir.
+              Path destPath = destination.resolve(projectPathInResources.relativize(path));
+              if (Files.isDirectory(path)) {
+                Files.createDirectory(destPath);
+              } else {
+                Files.copy(path, destPath);
+              }
+            });
+  }
+
+  private final String testProjectName;
+  private final String gradleVersion = GradleVersion.version("5.6.4").getVersion();
+  private GradleRunner gradleRunner;
+
+  private Path projectRoot;
+
+  /** Initialize with a specific project directory. */
+  public TestProject(String testProjectName) {
+    this.testProjectName = testProjectName;
+  }
+
+  @Override
+  public void close() {
+    after();
+  }
+
+  @Override
+  protected void before() throws Throwable {
+    super.before();
+
+    projectRoot = newFolder().toPath();
+    copyProject(testProjectName, projectRoot);
+
+    gradleRunner =
+        GradleRunner.create()
+            .withGradleVersion(gradleVersion)
+            .withProjectDir(projectRoot.toFile())
+            .withPluginClasspath();
+  }
+
+  public BuildResult build(String... gradleArguments) {
+    return gradleRunner.withArguments(gradleArguments).build();
+  }
+
+  public Path getProjectRoot() {
+    return projectRoot;
+  }
+}
