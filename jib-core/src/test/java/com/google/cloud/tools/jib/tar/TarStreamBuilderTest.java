11e744dc7cd2121039789e63d1bf7dab67775283
David Parry <david@spiraltechnology.co.uk>
2021-04-09 09:37:34 +0100

    Update jib-core/src/test/java/com/google/cloud/tools/jib/tar/TarStreamBuilderTest.java
    Co-authored-by: Chanseok Oh <chanseok@google.com>

--- jib-core/src/test/java/com/google/cloud/tools/jib/tar/TarStreamBuilderTest.java
+++ jib-core/src/test/java/com/google/cloud/tools/jib/tar/TarStreamBuilderTest.java
@@ -47,7 +47,6 @@ public class TarStreamBuilderTest {
   private byte[] fileAContents;
   private byte[] fileBContents;
   private TarStreamBuilder testTarStreamBuilder = new TarStreamBuilder();
-  private final Instant creationTime = Instant.EPOCH;
 
   @Before
   public void setup() throws URISyntaxException, IOException {
