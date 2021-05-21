859e840b529204daeb890ead7253ccac1bc8e46e
Mridula Peddada <mpeddada@google.com>
2021-05-21 14:57:07 -0400

    wip

---
+++ jib-cli/src/main/java/com/google/cloud/tools/jib/cli/War.java
@@ -0,0 +1,5 @@
+package com.google.cloud.tools.jib.cli;
+
+public class War {
+
+}
---
+++ jib-cli/src/main/java/com/google/cloud/tools/jib/cli/war/WarFiles.java
@@ -0,0 +1,36 @@
+package com.google.cloud.tools.jib.cli.war;
+
+import com.google.cloud.tools.jib.api.InvalidImageReferenceException;
+import com.google.cloud.tools.jib.api.Jib;
+import com.google.cloud.tools.jib.api.JibContainerBuilder;
+import com.google.cloud.tools.jib.api.buildplan.FileEntriesLayer;
+import com.google.cloud.tools.jib.cli.ArtifactProcessor;
+import com.google.cloud.tools.jib.cli.CommonCliOptions;
+import com.google.cloud.tools.jib.cli.Jar;
+import com.google.cloud.tools.jib.plugins.common.logging.ConsoleLogger;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WarFiles {
+
+    /**
+     * Generates a {@link JibContainerBuilder} from contents of a WAR file.
+     *
+     * @param processor artifact processor
+     * @param commonCliOptions common cli options
+     * @param logger console logger
+     * @return JibContainerBuilder
+     * @throws IOException if I/O error occurs when opening the jar file or if temporary directory
+     *     provided doesn't exist
+     * @throws InvalidImageReferenceException if the base image reference is invalid
+     */
+    public static JibContainerBuilder toJibContainerBuilder(ArtifactProcessor processor,  CommonCliOptions commonCliOptions, ConsoleLogger logger) throws IOException, InvalidImageReferenceException {
+        JibContainerBuilder containerBuilder = Jib.from("jetty");
+        List<FileEntriesLayer> layers = processor.createLayers();
+        List<String> entrypoint = processor.computeEntrypoint(new ArrayList<>());
+        containerBuilder.setEntrypoint(entrypoint).setFileEntriesLayers(layers);
+        return containerBuilder;
+    }
+}
