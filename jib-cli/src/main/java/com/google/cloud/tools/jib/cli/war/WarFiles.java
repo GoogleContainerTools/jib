2163590196e60be868a0c59428f5feb7ba3e4312
Mridula <66699525+mpeddada1@users.noreply.github.com>
2021-05-25 13:08:17 -0400

    Implement class to create JibContainerBuilder from the contents of a WAR file (#3248)
    * Implement class to create JibContainerBuilder from ArtifactProcessor and add tests

---
+++ jib-cli/src/main/java/com/google/cloud/tools/jib/cli/war/WarFiles.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.tools.jib.cli.war;
+
+import com.google.cloud.tools.jib.api.InvalidImageReferenceException;
+import com.google.cloud.tools.jib.api.Jib;
+import com.google.cloud.tools.jib.api.JibContainerBuilder;
+import com.google.cloud.tools.jib.cli.ArtifactProcessor;
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.util.List;
+
+public class WarFiles {
+
+  /**
+   * Generates a {@link JibContainerBuilder} from contents of a WAR file.
+   *
+   * @param processor artifact processor
+   * @return JibContainerBuilder
+   * @throws IOException if I/O error occurs when opening the war file or if temporary directory
+   *     provided doesn't exist
+   * @throws InvalidImageReferenceException if the base image reference is invalid
+   */
+  public static JibContainerBuilder toJibContainerBuilder(ArtifactProcessor processor)
+      throws IOException, InvalidImageReferenceException {
+    JibContainerBuilder containerBuilder = Jib.from("jetty");
+
+    // JVM Flags are ignored
+    List<String> entrypoint = processor.computeEntrypoint(ImmutableList.of());
+
+    containerBuilder.setEntrypoint(entrypoint).setFileEntriesLayers(processor.createLayers());
+    return containerBuilder;
+  }
+}
---
+++ jib-cli/src/test/java/com/google/cloud/tools/jib/cli/war/WarFilesTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2021 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.cloud.tools.jib.cli.war;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.when;
+
+import com.google.cloud.tools.jib.api.InvalidImageReferenceException;
+import com.google.cloud.tools.jib.api.JibContainerBuilder;
+import com.google.cloud.tools.jib.api.buildplan.AbsoluteUnixPath;
+import com.google.cloud.tools.jib.api.buildplan.ContainerBuildPlan;
+import com.google.cloud.tools.jib.api.buildplan.FileEntriesLayer;
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/** Tests for {@link WarFiles}. */
+@RunWith(MockitoJUnitRunner.class)
+public class WarFilesTest {
+
+  @Mock private StandardWarExplodedProcessor mockStandardWarExplodedProcessor;
+
+  @Test
+  public void testToJibContainerBuilder_explodedStandard_basicInfo()
+      throws IOException, InvalidImageReferenceException {
+    FileEntriesLayer layer =
+        FileEntriesLayer.builder()
+            .setName("classes")
+            .addEntry(
+                Paths.get("path/to/tempDirectory/WEB-INF/classes/class1.class"),
+                AbsoluteUnixPath.get("/my/app/WEB-INF/classes/class1.class"))
+            .build();
+    when(mockStandardWarExplodedProcessor.createLayers()).thenReturn(Arrays.asList(layer));
+    when(mockStandardWarExplodedProcessor.computeEntrypoint(anyList()))
+        .thenReturn(ImmutableList.of("java", "-jar", "/usr/local/jetty/start.jar"));
+
+    JibContainerBuilder containerBuilder =
+        WarFiles.toJibContainerBuilder(mockStandardWarExplodedProcessor);
+    ContainerBuildPlan buildPlan = containerBuilder.toContainerBuildPlan();
+
+    assertThat(buildPlan.getBaseImage()).isEqualTo("jetty");
+    assertThat(buildPlan.getEntrypoint())
+        .containsExactly("java", "-jar", "/usr/local/jetty/start.jar")
+        .inOrder();
+    assertThat(buildPlan.getLayers()).hasSize(1);
+    assertThat(buildPlan.getLayers().get(0).getName()).isEqualTo("classes");
+    assertThat(((FileEntriesLayer) buildPlan.getLayers().get(0)).getEntries())
+        .containsExactlyElementsIn(
+            FileEntriesLayer.builder()
+                .addEntry(
+                    Paths.get("path/to/tempDirectory/WEB-INF/classes/class1.class"),
+                    AbsoluteUnixPath.get("/my/app/WEB-INF/classes/class1.class"))
+                .build()
+                .getEntries());
+  }
+}
