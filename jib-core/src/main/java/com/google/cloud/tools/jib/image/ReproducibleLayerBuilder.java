641dd4fd3b368e9162b53c5336165ffb79919e91
Romain Manni-Bucau <rmannibucau@gmail.com>
2021-08-05 22:27:08 +0200

    Update jib-core/src/main/java/com/google/cloud/tools/jib/image/ReproducibleLayerBuilder.java
    Co-authored-by: Chanseok Oh <chanseok@google.com>

--- jib-core/src/main/java/com/google/cloud/tools/jib/image/ReproducibleLayerBuilder.java
+++ jib-core/src/main/java/com/google/cloud/tools/jib/image/ReproducibleLayerBuilder.java
@@ -176,6 +176,6 @@ public class ReproducibleLayerBuilder {
       tarStreamBuilder.addTarArchiveEntry(entry);
     }
 
-    return Blobs.from(tarStreamBuilder::writeAsTarArchiveTo, true);
+    return Blobs.from(tarStreamBuilder::writeAsTarArchiveTo, false);
   }
 }
