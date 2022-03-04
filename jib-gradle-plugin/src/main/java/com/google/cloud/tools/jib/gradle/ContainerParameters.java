07a3d7cd327c9db781c5cb35672796c357ee60e2
Duhuafei <biluohen@gmail.com>
2022-03-04 09:52:23 +0800

    fix format config on buildSrc kts srcipt
    in buildSrc srcipt: Cannot access class 'com.google.cloud.tools.jib.api.buildplan.ImageFormat'

--- jib-gradle-plugin/src/main/java/com/google/cloud/tools/jib/gradle/ContainerParameters.java
+++ jib-gradle-plugin/src/main/java/com/google/cloud/tools/jib/gradle/ContainerParameters.java
@@ -172,6 +172,10 @@ public class ContainerParameters {
   public void setFormat(ImageFormat format) {
     this.format = format;
   }
+  
+  public void setFormat(String format) {
+    this.format = ImageFormat.valueOf(format);
+  }
 
   @Input
   @Optional
