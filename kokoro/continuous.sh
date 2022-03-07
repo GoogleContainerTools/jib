536c4ed1e945b2d2ba584e33077aae37f6965fb9
Mridula Peddada <mpeddada@google.com>
2022-03-07 13:28:26 -0500

    modify continuous

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -6,9 +6,11 @@ set -o xtrace
 gcloud components install docker-credential-gcr
 
 # Docker service does not run by default in Big Sur but can be started with the following commands.
+if [ "${KOKORO_JOB_CLUSTER}" = "MACOS_EXTERNAL" ]; then
 docker-machine create --driver virtualbox default
 docker-machine env default
 eval "$(docker-machine env default)"
+fi
 
 # docker-credential-gcr uses GOOGLE_APPLICATION_CREDENTIALS as the credentials key file
 export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_KEYSTORE_DIR}/72743_jib_integration_testing_key
