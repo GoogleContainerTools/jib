cd2b25327e51bd8c3e95a7fce73471b53f67efe4
Mridula Peddada <mpeddada@google.com>
2022-03-07 13:27:01 -0500

    apply change for only macos

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -6,9 +6,11 @@ set -o xtrace
 gcloud components install docker-credential-gcr
 
 # Docker service does not run by default in Big Sur but can be started with the following commands.
+if [ "${KOKORO_JOB_CLUSTER}" = "MACOS_EXTERNAL" ]; then
 docker-machine create --driver virtualbox default
 docker-machine env default
 eval "$(docker-machine env default)"
+fi
 
 # Stops any left-over containers.
 docker stop $(docker ps --all --quiet) || true
