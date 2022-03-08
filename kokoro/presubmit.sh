223fbae308d93d5217ea2710cc0d1addb4b885f7
Mridula Peddada <mpeddada@google.com>
2022-03-08 16:12:48 -0500

    fix formatting

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -10,6 +10,10 @@ if [ "${KOKORO_JOB_CLUSTER}" = "MACOS_EXTERNAL" ]; then
 docker-machine create --driver virtualbox default
 docker-machine env default
 eval "$(docker-machine env default)"
+
+echo $JAVA_HOME
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-1.8.0_221.jdk/Contents/Home"
+export PATH=$JAVA_HOME/bin:$PATH
 fi
 
 # Stops any left-over containers.
@@ -17,11 +21,6 @@ docker stop $(docker ps --all --quiet) || true
 docker kill $(docker ps --all --quiet) || true
 
 cd github/jib
-
-echo $JAVA_HOME
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-1.8.0_221.jdk/Contents/Home
-export PATH=$JAVA_HOME/bin:$PATH
-
 
 # we only run integration tests on jib-core for presubmit
 ./gradlew clean build :jib-core:integrationTest --info --stacktrace
