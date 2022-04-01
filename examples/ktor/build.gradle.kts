00894f48ad5da37aab3df6ab36b3f28969a639fa
Elena Felder <41136058+elefeint@users.noreply.github.com>
2022-04-01 15:46:30 -0400

    update versions to 3.2.1 (#3629)

--- examples/ktor/build.gradle.kts
+++ examples/ktor/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
     kotlin("jvm") version "1.3.10"
-    id("com.google.cloud.tools.jib") version "3.2.0"
+    id("com.google.cloud.tools.jib") version "3.2.1"
 }
 
 group = "example"
