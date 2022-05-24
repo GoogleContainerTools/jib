f09a23f51e056eac12901aff19b9c8fda8fa3d5e
Wallace Wadge <wwadge@gmail.com>
2022-05-24 08:56:18 +0200

    Fix buildImage_writesImageJson test by adding a mock

--- jib-plugins-common/src/test/java/com/google/cloud/tools/jib/plugins/common/JibBuildRunnerTest.java
+++ jib-plugins-common/src/test/java/com/google/cloud/tools/jib/plugins/common/JibBuildRunnerTest.java
@@ -242,6 +242,7 @@ public class JibBuildRunnerTest {
     Mockito.when(mockJibContainer.getTags()).thenReturn(tags);
     Mockito.when(mockJibContainerBuilder.containerize(mockContainerizer))
         .thenReturn(mockJibContainer);
+    Mockito.when(mockJibContainer.isImagePushed()).thenReturn(true);
     testJibBuildRunner.writeImageJson(outputPath).runBuild();
 
     final String outputJson = new String(Files.readAllBytes(outputPath), StandardCharsets.UTF_8);
