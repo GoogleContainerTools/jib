d833ea622d7b002ebf29218ff5d00714493d7952
Mridula Peddada <mpeddada@google.com>
2022-06-03 11:25:06 -0400

    add insecure registries option

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -9,6 +9,9 @@ gcloud components install docker-credential-gcr
 if [ "${KOKORO_JOB_CLUSTER}" = "MACOS_EXTERNAL" ]; then
 docker-machine ls
 docker-machine start default
+docker-machine ssh default "echo '{ \"insecure-registries\":[\"192.168.99.104:5000\"] }' | sudo tee /etc/docker/daemon.json "
+docker-machine ssh default "echo 'DOCKER_OPTS=\"--config-file=/etc/docker/daemon.json\"' | sudo tee -a /var/lib/boot2docker/profile "
+docker-machine ssh default "sudo /etc/init.d/docker restart"
 docker-machine env default
 eval "$(docker-machine env default)"
 export DOCKER_IP="$(docker-machine ip default)"
