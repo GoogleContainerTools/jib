ae1edd0b6f04d08a4f6c825cee20e5d707ae0c21
Mridula Peddada <mpeddada@google.com>
2022-06-06 22:07:16 -0400

    add port to script

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -11,7 +11,7 @@ docker-machine ls
 docker-machine start default
 export DOCKER_IP="$(docker-machine ip default)"
 echo $DOCKER_IP
-docker-machine ssh default "echo '{ \"insecure-registries\":[\"$DOCKER_IP\"] }' | sudo tee /etc/docker/daemon.json "
+docker-machine ssh default "echo '{ \"insecure-registries\":[\"$DOCKER_IP:5000, $DOCKER_IP:8080\"] }' | sudo tee /etc/docker/daemon.json "
 docker-machine ssh default "echo 'DOCKER_OPTS=\"--config-file=/etc/docker/daemon.json\"' | sudo tee -a /var/lib/boot2docker/profile "
 docker-machine ssh default "sudo /etc/init.d/docker restart"
 docker-machine env default
