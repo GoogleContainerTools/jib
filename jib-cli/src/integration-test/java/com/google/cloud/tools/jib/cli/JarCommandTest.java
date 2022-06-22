ce193cf4a93eb2b755ce033fb4fc565be6ca0b0f
Elena Felder <41136058+elefeint@users.noreply.github.com>
2022-06-22 10:24:54 -0400

    Use temurin builds based on older version of ubuntu (#3694)
    * latest versions of temurin are incompatible with older versions of docker

--- jib-cli/src/integration-test/java/com/google/cloud/tools/jib/cli/JarCommandTest.java
+++ jib-cli/src/integration-test/java/com/google/cloud/tools/jib/cli/JarCommandTest.java
@@ -87,7 +87,13 @@ public class JarCommandTest {
     Path jarPath = Paths.get(Resources.getResource("jarTest/standard/jarWithCp.jar").toURI());
     Integer exitCode =
         new CommandLine(new JibCli())
-            .execute("jar", "--target", "docker://exploded-jar", jarPath.toString());
+            .execute(
+                "jar",
+                "--from",
+                "eclipse-temurin:8-jdk-focal",
+                "--target",
+                "docker://exploded-jar",
+                jarPath.toString());
     String output = new Command("docker", "run", "--rm", "exploded-jar").run();
     try (JarFile jarFile = new JarFile(jarPath.toFile())) {
       String classPath =
@@ -105,7 +111,13 @@ public class JarCommandTest {
     Path jarPath = Paths.get(Resources.getResource("jarTest/standard/noDependencyJar.jar").toURI());
     Integer exitCode =
         new CommandLine(new JibCli())
-            .execute("jar", "--target", "docker://exploded-no-dep-jar", jarPath.toString());
+            .execute(
+                "jar",
+                "--from",
+                "eclipse-temurin:8-jdk-focal",
+                "--target",
+                "docker://exploded-no-dep-jar",
+                jarPath.toString());
     String output = new Command("docker", "run", "--rm", "exploded-no-dep-jar").run();
     try (JarFile jarFile = new JarFile(jarPath.toFile())) {
       String classPath =
@@ -124,7 +136,13 @@ public class JarCommandTest {
     Integer exitCode =
         new CommandLine(new JibCli())
             .execute(
-                "jar", "--target", "docker://packaged-jar", jarPath.toString(), "--mode=packaged");
+                "jar",
+                "--from",
+                "eclipse-temurin:8-jdk-focal",
+                "--target",
+                "docker://packaged-jar",
+                jarPath.toString(),
+                "--mode=packaged");
     String output = new Command("docker", "run", "--rm", "packaged-jar").run();
     try (JarFile jarFile = new JarFile(jarPath.toFile())) {
       String classPath =
@@ -144,6 +162,8 @@ public class JarCommandTest {
         new CommandLine(new JibCli())
             .execute(
                 "jar",
+                "--from",
+                "eclipse-temurin:8-jdk-focal",
                 "--target",
                 "docker://packaged-no-dep-jar",
                 jarPath.toString(),
@@ -167,7 +187,13 @@ public class JarCommandTest {
 
     Integer exitCode =
         new CommandLine(new JibCli())
-            .execute("jar", "--target", "docker://spring-boot-jar-layered", jarPath.toString());
+            .execute(
+                "jar",
+                "--from",
+                "eclipse-temurin:8-jdk-focal",
+                "--target",
+                "docker://spring-boot-jar-layered",
+                jarPath.toString());
     assertThat(exitCode).isEqualTo(0);
 
     String output =
@@ -189,7 +215,13 @@ public class JarCommandTest {
 
     Integer exitCode =
         new CommandLine(new JibCli())
-            .execute("jar", "--target", "docker://spring-boot-jar", jarPath.toString());
+            .execute(
+                "jar",
+                "--from",
+                "eclipse-temurin:8-jdk-focal",
+                "--target",
+                "docker://spring-boot-jar",
+                jarPath.toString());
     assertThat(exitCode).isEqualTo(0);
 
     String output =
@@ -211,6 +243,8 @@ public class JarCommandTest {
         new CommandLine(new JibCli())
             .execute(
                 "jar",
+                "--from",
+                "eclipse-temurin:8-jdk-focal",
                 "--target",
                 "docker://packaged-spring-boot",
                 jarPath.toString(),
