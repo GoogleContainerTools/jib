a75c38c43ed8c5ead3c2e5a535a10ea267567a3c
Mridula Peddada <mpeddada@google.com>
2022-07-06 15:08:29 -0400

    copy auth file into vm

--- kokoro/docker_setup.sh
+++ kokoro/docker_setup.sh
@@ -8,6 +8,7 @@ echo $DOCKER_IP
 docker-machine ssh default "echo '{ \"insecure-registries\":[\"$DOCKER_IP:5000\", \"$DOCKER_IP:8080\"] }' | sudo tee /etc/docker/daemon.json "
 docker-machine ssh default "echo 'DOCKER_OPTS=\"--config-file=/etc/docker/daemon.json\"' | sudo tee -a /var/lib/boot2docker/profile "
 docker-machine ssh default 'mkdir /home/docker/auth; docker run --entrypoint htpasswd httpd:2 -Bbn testuser testpassword > /home/docker/auth/htpasswd'
+docker-machine scp -r auth docker@default:/home/docker/auth
 docker-machine ssh default "sudo /etc/init.d/docker restart"
 docker-machine env default
 eval "$(docker-machine env default)"
