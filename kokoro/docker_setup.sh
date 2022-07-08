1497e44351a7d11da50ff5bb2b9ba1ee5987df72
Mridula Peddada <mpeddada@google.com>
2022-07-08 17:37:00 -0400

    add port 6000 to insecure registries list

--- kokoro/docker_setup.sh
+++ kokoro/docker_setup.sh
@@ -5,7 +5,7 @@ docker-machine start default
 export DOCKER_IP="$(docker-machine ip default)"
 echo $DOCKER_IP
 
-docker-machine ssh default "echo '{ \"insecure-registries\":[\"$DOCKER_IP:5000\", \"$DOCKER_IP:8080\"] }' | sudo tee /etc/docker/daemon.json "
+docker-machine ssh default "echo '{ \"insecure-registries\":[\"$DOCKER_IP:5000\",\"$DOCKER_IP:6000\",\"$DOCKER_IP:8080\"] }' | sudo tee /etc/docker/daemon.json "
 docker-machine ssh default "echo 'DOCKER_OPTS=\"--config-file=/etc/docker/daemon.json\"' | sudo tee -a /var/lib/boot2docker/profile "
 docker-machine ssh default "mkdir /home/docker/auth; docker run --entrypoint htpasswd httpd:2 -Bbn testuser testpassword > /home/docker/auth/htpasswd"
 docker-machine ssh default "cat /home/docker/auth/htpasswd"
