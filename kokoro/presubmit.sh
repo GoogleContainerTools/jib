64647e754bfb4d2079bfc55e851c3dd4219733fa
Mridula Peddada <mpeddada@google.com>
2022-10-24 16:29:26 -0400

    run just SingleProjectIntegrationTest#testDockerDaemon_containerWithTruststoreJvmArgument

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -17,4 +17,4 @@ docker kill $(docker ps --all --quiet) || true
 cd github/jib
 
 # we only run integration tests on jib-core for presubmit
-./gradlew clean build :jib-core:integrationTest --info --stacktrace
+./gradlew clean build :jib-gradle-plugin:integrationTest --tests "com.google.cloud.tools.jib.gradle.SingleProjectIntegrationTest.testDockerDaemon_containerWithTruststoreJvmArgument" --info --stacktrace
