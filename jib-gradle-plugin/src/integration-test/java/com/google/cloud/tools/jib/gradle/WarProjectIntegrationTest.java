fa33a4c9627dae60d2cf9e1dc386f8f987bbe4da
Emily Wang <emmwang@google.com>
2023-04-04 17:26:15 -0400

    Try container IP workaround in gradle plugin integration tests with web apps

--- jib-gradle-plugin/src/integration-test/java/com/google/cloud/tools/jib/gradle/WarProjectIntegrationTest.java
+++ jib-gradle-plugin/src/integration-test/java/com/google/cloud/tools/jib/gradle/WarProjectIntegrationTest.java
@@ -18,9 +18,12 @@ package com.google.cloud.tools.jib.gradle;
 
 import com.google.cloud.tools.jib.Command;
 import com.google.cloud.tools.jib.IntegrationTestingConfiguration;
+import com.google.common.collect.Lists;
 import java.io.IOException;
 import java.net.URL;
 import java.security.DigestException;
+import java.util.List;
+import java.util.logging.Logger;
 import javax.annotation.Nullable;
 import org.junit.After;
 import org.junit.Assert;
@@ -29,6 +32,8 @@ import org.junit.Test;
 
 /** Integration tests for building WAR images. */
 public class WarProjectIntegrationTest {
+
+  private static final Logger LOGGER = Logger.getLogger(WarProjectIntegrationTest.class.getName());
 
   @ClassRule public static final TestProject servlet25Project = new TestProject("war_servlet25");
 
@@ -62,8 +67,44 @@ public class WarProjectIntegrationTest {
     String output =
         JibRunHelper.buildAndRun(project, targetImage, gradleBuildFile, "--detach", "-p8080:8080");
     containerName = output.trim();
-
+    LOGGER.info("Container name: " + containerName);
+    if (System.getenv("KOKORO_JOB_CLUSTER") != null
+        && System.getenv("KOKORO_JOB_CLUSTER").equals("GCP_UBUNTU_DOCKER")) {
+      String containerIp = getAndMapContainerIp(containerName);
+      LOGGER.info("Mapped registry container IP to localhost: " + containerIp);
+    }
     Assert.assertEquals(
         "Hello world", JibRunHelper.getContent(new URL("http://" + dockerHost + ":8080/hello")));
+  }
+
+  /** Gets container IP and associates it to localhost. */
+  private String getAndMapContainerIp(String containerName) {
+    String containerIp;
+
+    // Gets local registry container IP
+    List<String> dockerTokens =
+        Lists.newArrayList(
+            "docker",
+            "inspect",
+            "-f",
+            "'{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}'",
+            containerName);
+    try {
+      String result = new Command(dockerTokens).run();
+      // Remove single quotes and LF from result (e.g. '127.0.0.1'\n)
+      containerIp = result.replaceAll("['\n]", "");
+    } catch (InterruptedException | IOException ex) {
+      throw new RuntimeException("Could get container IP for: " + containerName, ex);
+    }
+
+    // Associate container IP with localhost
+    try {
+      String addHost =
+          new Command("bash", "-c", "echo \"" + containerIp + " localhost\" >> /etc/hosts").run();
+    } catch (InterruptedException | IOException ex) {
+      throw new RuntimeException("Could not associate container IP to localhost: " + containerIp);
+    }
+
+    return containerIp;
   }
 }
