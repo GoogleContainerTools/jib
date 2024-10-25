aac44d35da214e55cc2b98ed138b84737e061005
Mridula <66699525+mpeddada1@users.noreply.github.com>
2024-10-25 18:30:31 -0400

    fix: add timeout for docker info read to prevent Jib from getting stuck indefinitely (#4319)
    * fix: address nightly build failure with dockerBuild; add timeout for docker info read

--- jib-gradle-plugin/src/integration-test/resources/gradle/projects/simple/mock-docker.sh
+++ jib-gradle-plugin/src/integration-test/resources/gradle/projects/simple/mock-docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$1" == "info" ]]; then
+  # Output the JSON string
+  echo "{\"OSType\":\"linux\",\"Architecture\":\"x86_64\"}"
+  exit 0
+fi
+
 # Read stdin to avoid broken pipe
 cat > /dev/null
 
